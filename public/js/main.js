69178fb36b5ebc68b61e7285f435013cbcf54980
Tate <tate.donne@gmail.com>
2023-08-31 19:21:54 -0400

    CSS styling and adjustment

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,14 @@
 // FRONT-END (CLIENT) JAVASCRIPT HERE
 
-const submit = async function( event ) {
+const submit = async function( event , selectorString) {
   // stop form submission from trying to load
   // a new .html page for displaying results...
   // this was the original browser behavior and still
   // remains to this day
   event.preventDefault()
 
-  const input = document.querySelector( '#yourname' ),
-      json = { yourname: input.value },
+  const input = document.querySelector( "input" ),
+      json = { selector: input.value },
       body = JSON.stringify( json )
 
   const response = await fetch( '/submit', {
