a508ec5f89950e84f3415dd13e55ca76f7a835c3
Matthew McAlarney <67719286+MacCode7110@users.noreply.github.com>
2023-09-01 12:57:45 -0400

    Added new input fields to form

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,52 @@
 // FRONT-END (CLIENT) JAVASCRIPT HERE
+//For form functionality - so posting new data to the server - we want to use async and await
 
-const submit = async function( event ) {
-  // stop form submission from trying to load
-  // a new .html page for displaying results...
-  // this was the original browser behavior and still
-  // remains to this day
+const submit = async function(event) { //The async keyword here means that submit always returns a promise. It also means that one or more await keywords are allowed inside the function body.
+  
+  //preventDefault cancels an event as long as it is cancelable.
+  //stop form submission from trying to load
+  //a new .html page for displaying results...
+  //this was the original browser behavior and still
+  //remains to this day
   event.preventDefault()
   
-  const input = document.querySelector( '#yourname' ),
-        json = { yourname: input.value },
-        body = JSON.stringify( json )
+  const input = document.querySelector('#yourname'), //query the HTML file for the first element that uses id yourname
+        json = {yourname: input.value}, //declare a json constant using a key value pair with the key being yourname and the value being the value of the input HTML element queried in the document.
+        body = JSON.stringify(json) //stringify converts a value to a JSON string. Assign the body to this JSON string.
 
-  const response = await fetch( '/submit', {
-    method:'POST',
-    body 
+  //Take in three different inputs
+  //Append strings from JSON to assign body - all form inputs  
+  //git ignore node modules
+
+  //await works only inside async functions. 
+  //await keyword makes JavaScript wait until the promise resolves.
+  //fetch method goes as follows: let promise = fetch(url, [options])
+    //URL
+    //Optional parameters include method, headers, and more.
+  
+  //We have two promises here:
+    //The response to the form submit
+    //The text of the response body
+
+  const response = await fetch('/submit', { //wait until the fetch of the response to the form submit resolves to store the response.
+    method:'POST', //pass in the HTTP method (so sending new data to server through POST)
+    body //pass in the response body
   })
+  
+  const data = await response.json()
 
-  const text = await response.text()
-
-  console.log( 'text:', text )
+  const list = document.createElement('ul')
+  
+  data.forEach(d => {
+    const item = document.createElement('li')
+    item.innerHTML = `<strong>Band Name</strong>: ${d.bandName}, <strong>Album Name</strong>: ${d.albumName}, <strong>Release Year</strong>: ${d.releaseYear}` //Template literal example
+    list.appendChild(item)
+  })
+  
+  document.body.appendChild(list)
 }
 
-window.onload = function() {
+window.onload = function() { //At the time the window loads, query the HTML document for the first button element. Then, on the left-mouse click of the button submit the form.
    const button = document.querySelector("button");
-  button.onclick = submit;
+   button.onclick = submit;
 }
--- server.improved.js
+++ server.improved.js
@@ -1,74 +1,97 @@
-const http = require( 'http' ),
-      fs   = require( 'fs' ),
+const http = require('http'), //Allow use of HTTP server and client
+      fs   = require('fs'), //Enables interaction with the file system
       // IMPORTANT: you must run `npm install` in the directory for this assignment
       // to install the mime library if you're testing this on your local machine.
       // However, Glitch will install it automatically by looking in your package.json
       // file.
-      mime = require( 'mime' ),
+      mime = require('mime'),
       dir  = 'public/',
       port = 3000
 
-const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
+let musicListeningData = [
+  { 'bandName': 'Dry Kill Logic', 'albumName': 'The Darker Side of Nonsense', 'releaseYear': '2001'},
+  { 'bandName': 'Dry Kill Logic', 'albumName': 'The Dead and Dreaming', 'releaseYear': '2004'},
+  { 'bandName': 'Killswitch Engage', 'albumName': 'Alive or Just Breathing', 'releaseYear': '2002'} 
 ]
 
-const server = http.createServer( function( request,response ) {
-  if( request.method === 'GET' ) {
-    handleGet( request, response )    
-  }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
+//Personal notes:
+//HTTP requests allow interaction between frontend and backend of websites
+//There must be an interaction betwwen a server and client to modify data
+//HTTP methods to request data allows us to perform CRUD operations on servers: POST, GET, PUT/PATCH, and DELETE.
+//Every request includes an endpoint and a type of request being sent.
+//The endpoint is the path between the client and server.
+//Server sends back a response depending on the validity of the request
+  //If the request is successful, the server sends back data in the form of JSON.
+  //If the request fails, then an error message is sent back to the client.
+  //Responses are associated with status codes:
+    //A status code of 200 means that the request was successful.
+//Client sends the request, server recieves the request.
+
+//GET: GET Request is used to retrieve data from the server.
+//POST: POST Request sends new data to the server as an object.
+//PATCH: PATCH Request is used to update certain properties of an object.
+//PUT: PUT Request updates all properties of an object.
+//DELETE: DELETE Request deletes data from a server.
+
+const server = http.createServer(function(request,response) { //Create an HTTP server. RequestListener is automatically added to the Request event when the server is created.
+  if(request.method === 'GET') { //If the client is requesting retrieval of data from the server
+    handleGet(request, response)    
+  }else if(request.method === 'POST'){ //If client is requesting to send new data to server
+    handlePost(request, response) 
   }
 })
 
-const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
 
-  if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
-  }else{
-    sendFile( response, filename )
+//Handle the GET and POST requests in separate cases:
+
+const handleGet = function(request, response) {
+  const filename = dir + request.url.slice(1) //The slice() method returns selected elements in an array, as a new array. Selects from a given starting index up to (not including) a given ending index.
+
+  if(request.url === '/') { //If the request URL is to the index.html file
+    sendFile(response, 'public/index.html')
+  }else{ //If the request URL is to any other file under public folder
+    sendFile(response, filename)
   }
 }
 
-const handlePost = function( request, response ) {
+const handlePost = function(request, response) {
   let dataString = ''
 
-  request.on( 'data', function( data ) {
+  request.on('data', function(data) { //When client is requesting to send new data to server, append the data to dataString.
       dataString += data 
   })
 
-  request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
+  request.on('end', function() { //At the end of a request, do the following:
+    console.log(JSON.parse(dataString)) //JSON.parse converts a JSON string into an object
 
     // ... do something with the data here!!!
 
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end('test')
+    response.writeHead(200, "OK", {'Content-Type': 'text/json'}) //writeHead sends a response header to the client request. Here, we send a 200 status OK response header.
+    response.end(JSON.stringify(musicListeningData)) //End the response process with the appdata converted to a JSON string.
   })
 }
 
-const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
+const sendFile = function(response, filename) { //sendFile function only applies to the GET Request as this is where the client requests data from the server (therefore a file needs to be sent to the client).
+   const type = mime.getType(filename) //Get the file type
 
-   fs.readFile( filename, function( err, content ) {
+   fs.readFile(filename, function(err, content) { //read the file using fs and check for errors.
 
      // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
+     if(err === null) {
 
        // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
+       response.writeHeader(200, {'Content-Type': type}) //Send a response header to the client request with an OK status 200 and include the file/content type.
+       response.end(content) //End the response process with the file contents.
 
-     }else{
+     } else {
 
        // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
+       response.writeHeader(404)
+       response.end('404 Error: File Not Found')
 
      }
    })
 }
 
-server.listen( process.env.PORT || port )
+server.listen(process.env.PORT || port) //asynchronous function, make the server created above start listening for connections.
+console.log("Server is listening on port: ", server.address().port)
