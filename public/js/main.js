816d45489a658c0ac4adbddc3884a39330fcb3f9
Mirandi McCormick <mirandicat@att.net>
2023-09-04 13:20:06 -0400

    1 field form working

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,7 @@
 // FRONT-END (CLIENT) JAVASCRIPT HERE
 
+const list = document.createElement('ul')
+
 const submit = async function( event ) {
   // stop form submission from trying to load
   // a new .html page for displaying results...
@@ -18,18 +20,28 @@ const submit = async function( event ) {
 
   const jsonData = await response.json()
 
-  const list = document.createElement('ul')
-
   jsonData.forEach(e => {
     const item = document.createElement('li')
     item.innerText = `Fact : ${e.addfact}`
     list.appendChild(item)
+
+    const delItem = document.createElement('span');
+    delItem.innerText = 'x'
+    item.appendChild(delItem)
   });
 
   document.body.appendChild(list)
   
+  const itemToDelete = document.querySelectorAll('span')
+  itemToDelete.forEach(e => {
+    e.addEventListener('click', ()=>{
+      e.parentElement.remove()
+    })
+  });
 
 }
+
+
 
 window.onload = function() {
    const button = document.querySelector("button");
