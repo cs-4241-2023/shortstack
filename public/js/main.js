e292c5652fee4d57228ad06fba997eb5dc4dc6c2
Engjell Ramadani <eramadani@wpi.edu>
2023-09-06 22:50:47 -0400

    Updated todo list and got server to take in tasks

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,60 @@
 // FRONT-END (CLIENT) JAVASCRIPT HERE
 
-const submit = async function( event ) {
-  // stop form submission from trying to load
-  // a new .html page for displaying results...
-  // this was the original browser behavior and still
-  // remains to this day
-  event.preventDefault()
-  
-  const input = document.querySelector( '#yourname' ),
-        json = { yourname: input.value },
-        body = JSON.stringify( json )
+// Function to fetch tasks from the server
+const fetchTasks = async function () {
+  try {
+    const response = await fetch('/tasks');
+    if (!response.ok) {
+      throw new Error('Failed to fetch tasks');
+    }
+    const tasks = await response.json();
+    displayTasks(tasks);
+  } catch (error) {
+    console.error('Error fetching tasks:', error);
+  }
+};
 
-  const response = await fetch( '/submit', {
-    method:'POST',
-    body 
-  })
+// Function to display tasks in the todo list
+const displayTasks = function (tasks) {
+  const taskList = document.getElementById('task-list');
+  taskList.innerHTML = ''; // Clear the existing list
 
-  const text = await response.text()
+  tasks.forEach((task) => {
+    const listItem = document.createElement('li');
+    listItem.textContent = task.task; // Use 'task' instead of 'yourname'
+    taskList.appendChild(listItem);
+  });
+};
 
-  console.log( 'text:', text )
-}
+// Function to handle form submission
+const submit = async function (event) {
+  event.preventDefault();
 
-window.onload = function() {
-   const button = document.querySelector("button");
+  const input = document.querySelector('#task-input'),
+    json = { task: input.value },
+    body = JSON.stringify(json);
+
+  const response = await fetch('/submit', { // This is where you specify the endpoint
+    method: 'POST',
+    body,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (response.ok) {
+    // If the task was added successfully, fetch and display the updated list of tasks
+    fetchTasks();
+  } else {
+    console.error('Failed to add task');
+  }
+};
+
+// Fetch and display tasks when the page loads
+window.onload = function () {
+  const button = document.querySelector('button');
   button.onclick = submit;
-}
+
+  // Fetch and display tasks from the server
+  fetchTasks();
+};
--- server.improved.js
+++ server.improved.js
@@ -8,11 +8,8 @@ const http = require( 'http' ),
       dir  = 'public/',
       port = 3000
 
-const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
-]
+const tasks = [];
+
 
 const server = http.createServer( function( request,response ) {
   if( request.method === 'GET' ) {
@@ -22,53 +19,59 @@ const server = http.createServer( function( request,response ) {
   }
 })
 
-const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
+const handlePost = function (request, response) {
+  let dataString = '';
 
-  if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
-  }else{
-    sendFile( response, filename )
+  request.on('data', function (data) {
+    dataString += data;
+  });
+
+  request.on('end', function () {
+    const newTask = JSON.parse(dataString);
+    newTask.timestamp = new Date(); // Add a timestamp to the task
+    tasks.push(newTask);
+
+    console.log('New Task:', newTask);
+
+    response.writeHead(200, 'OK', { 'Content-Type': 'text/plain' });
+    response.end('Task added successfully');
+  });
+};
+
+
+const handleGet = function (request, response) {
+  const filename = dir + request.url.slice(1);
+
+  if (request.url === '/') {
+    sendFile(response, 'public/index.html');
+  } else if (request.url === '/tasks') { // New endpoint for tasks
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    response.end(JSON.stringify(tasks));
+  } else {
+    sendFile(response, filename);
   }
-}
+};
 
-const handlePost = function( request, response ) {
-  let dataString = ''
 
-  request.on( 'data', function( data ) {
-      dataString += data 
-  })
 
-  request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
+const sendFile = function (response, filename) {
+  const type = mime.getType(filename);
 
-    // ... do something with the data here!!!
+  if (filename === 'public/index.html') {
+    fs.readFile(filename, function (err, content) {
+      if (err === null) {
+        response.writeHeader(200, { 'Content-Type': type });
+        response.end(content);
+      } else {
+        response.writeHeader(404);
+        response.end('404 Error: File Not Found');
+      }
+    });
+  } else {
+    // Handle other file types or endpoints as needed
+    // You can keep the existing code here
+  }
+};
 
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end('test')
-  })
-}
-
-const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
-
-   fs.readFile( filename, function( err, content ) {
-
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
-
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
-
-     }else{
-
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
-
-     }
-   })
-}
 
 server.listen( process.env.PORT || port )
