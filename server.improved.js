d4d7efa83d3eefb38c2a5c19a421573b3c4a8b4d
Mirandi McCormick <mirandicat@att.net>
2023-09-07 19:54:04 -0400

    added all creature images

--- server.improved.js
+++ server.improved.js
@@ -16,6 +16,14 @@ const maxAges = {"Chameleon": 16, "Gecko": 20,
                  "Frog": 20, "Snake": 30,
                  "Cat": 20, "Dog": 15,
                  "Rat": 4, "Capybara": 12,}
+const creaturePics = {"Chameleon": 'https://lafeber.com/vet/wp-content/uploads/Veiled-chameleon-by-Mrs-Logic-cropped-square.jpg', 
+                      "Gecko": 'https://reptile.guide/wp-content/uploads/2021/03/Leopardengecko-eublepharis-macularius.jpg',
+                      "Frog": 'https://www.sonova.com/sites/default/files/styles/header_image_tablet/public/2019-07/shutterstock_253580635_square.jpg?itok=TwBeTHTY', 
+                      "Snake": 'https://static.wixstatic.com/media/a96e12_2bfd46ea53944c979c805a5a50327cb9~mv2.jpg/v1/fit/w_500,h_500,q_90/file.jpg',
+                      "Cat": 'https://media.discordapp.net/attachments/1149381126847201290/1149489057265623211/PXL_20230902_231104485.jpg?width=443&height=468', 
+                      "Dog": 'https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_square.jpg',
+                      "Rat": 'https://images.squarespace-cdn.com/content/v1/55801f1be4b0bd4b73b60d65/1449930415127-EU3SDWQQMOO6LV0Y0QJF/rat+square.jpg', 
+                      "Capybara": 'https://gvzoo.com/cms-data/gallery/blog/animals/capybara/banner-capybara-sq.jpg',}
 
 const server = http.createServer( function( request,response ) {
   if( request.method === 'GET' ) {
@@ -51,6 +59,9 @@ const handlePost = function( request, response ) {
                    status: '',
                    picture: ''}
 
+
+    json.picture = creaturePics[stuff.type];
+
     if(stuff.age <= avgAges[stuff.type] / 2){
       json.status = "your creature is young!"
     }else if(stuff.age >= maxAges[stuff.type]){
@@ -58,8 +69,6 @@ const handlePost = function( request, response ) {
     }else{
       json.status = "your creature is middle aged"
     }
-
-    json.picture = 'https://media.discordapp.net/attachments/1019236301423247444/1144700810366816287/20230824_210454.jpg?width=474&height=1026'
 
     console.log(json)
 
