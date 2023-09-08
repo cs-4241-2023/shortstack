b4adb2340945e6061d805d704cbba66c71944bd6
Andrew Hariyanto <51850304+andrewhariyanto@users.noreply.github.com>
2023-09-08 01:13:18 -0400

    baseline achievement

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,70 @@
 // FRONT-END (CLIENT) JAVASCRIPT HERE
 
-const submit = async function( event ) {
+const submit = async function (event) {
+  console.log('passed3');
   // stop form submission from trying to load
   // a new .html page for displaying results...
   // this was the original browser behavior and still
   // remains to this day
   event.preventDefault()
-  
-  const input = document.querySelector( '#yourname' ),
-        json = { yourname: input.value },
-        body = JSON.stringify( json )
 
-  const response = await fetch( '/submit', {
-    method:'POST',
-    body 
+  const keyInput = document.querySelector('#key-input'),
+    startInput = document.querySelector('#start-date-input'),
+    endInput = document.querySelector('#end-date-input'),
+    json = { key: keyInput.value, startDate: startInput.value, endDate: endInput.value },
+    body = JSON.stringify(json);
+
+  console.log('passed2');
+  const response = await fetch('/submit', {
+    method: 'POST',
+    body
   })
 
-  const text = await response.text()
+  const data = await response.json()
 
-  console.log( 'text:', text )
+  console.log('text:', data)
+  displayResults(data);
 }
 
-window.onload = function() {
-   const button = document.querySelector("button");
-  button.onclick = submit;
+const firstLoad = async function () {
+  const response = await fetch("/getData");
+
+  const data = await response.json();
+
+  displayResults(data);
+}
+
+const displayResults = (data) => {
+  console.log(data.map(d => d.key));
+  document.getElementById('data').innerHTML = '';
+
+  for (let i = 0; i < data.length; i++) {
+    let tableRow = document.createElement('tr');
+    let keyColumn = document.createElement('td'),
+      startDateColumn = document.createElement('td'),
+      endDateColumn = document.createElement('td'),
+      dueDateColumn = document.createElement('td');
+
+    let textKey = document.createTextNode(data[i].key),
+      textStart = document.createTextNode(data[i].startDate),
+      textEnd = document.createTextNode(data[i].endDate),
+      start = new Date(data[i].startDate),
+      end = new Date(data[i].endDate),
+      due = end.getTime() - start.getTime(),
+      textDiff = document.createTextNode(due / (1000 * 60 * 60 * 24));
+    keyColumn.appendChild(textKey);
+    startDateColumn.appendChild(textStart);
+    endDateColumn.appendChild(textEnd);
+    dueDateColumn.appendChild(textDiff);
+    tableRow.appendChild(keyColumn);
+    tableRow.appendChild(startDateColumn);
+    tableRow.appendChild(endDateColumn);
+    tableRow.appendChild(dueDateColumn);
+    document.querySelector('#data').appendChild(tableRow);
+  }
+}
+
+window.onload = function () {
+  document.querySelector('#taskForm').onsubmit = submit;
+  firstLoad();
 }
