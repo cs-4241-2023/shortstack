63ca4b906f4c5cb8a93cd738a4fe2a8e367b03bd
apblanchard4 <107159438+apblanchard4@users.noreply.github.com>
2023-09-09 10:52:58 -0400

    Basic functionality

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,66 @@
-// FRONT-END (CLIENT) JAVASCRIPT HERE
-
-const submit = async function( event ) {
-  // stop form submission from trying to load
-  // a new .html page for displaying results...
-  // this was the original browser behavior and still
-  // remains to this day
+const submit = async function (event) {
   event.preventDefault()
-  
-  const input = document.querySelector( '#yourname' ),
-        json = { yourname: input.value },
-        body = JSON.stringify( json )
+  const title = document.querySelector("#title");
+  const author = document.querySelector("#author");
+  const started = document.querySelector("#started");
+  const finished = document.querySelector("#finished");
+  const rating = document.querySelector("#rating");
 
-  const response = await fetch( '/submit', {
-    method:'POST',
-    body 
+  if (title.value == '' || author.value == '' || started.value == '' || finished.value == '' || rating.value == '' || summary.value == '') {
+    alert('Please fill in all fields');
+    return false;
+  }
+
+
+  json = {
+    title: title.value,
+    author: author.value,
+    started: started.value,
+    finished: finished.value,
+    rating: rating.value,
+  },
+    body = JSON.stringify(json)
+
+  const response = await fetch('/submit', {
+    method: 'POST',
+    body
   })
-
-  const text = await response.text()
-
-  console.log( 'text:', text )
+  const data = await response.json()
+  updateTable(data);
+}
+const removeRow = async function (bookTitle) {
+  let json = { titleToRemove: bookTitle }
+  let body = JSON.stringify(json)
+  fetch('/remove', {
+    method: 'DELETE',
+    body
+  }).then(response => response.json())
+    .then(json => {
+      updateTable(json)
+    })
+  return false;
 }
 
-window.onload = function() {
-   const button = document.querySelector("button");
-  button.onclick = submit;
+
+const updateTable = function (jsonData) {
+  const table = document.querySelector("table")
+  console.log(table)
+  table.innerHTML = "<tr> <th>Title</th> <th>Author</th> <th>Started</th><th>Finished</th><th>Days taken to Read</th><th>Rating</th><th>Delete</th></tr>"
+
+
+  jsonData.forEach(entry => {
+    table.innerHTML += `<tr> <th>${entry.title}</th> <th>${entry.author}</th> <th>${entry.started}</th> <th>${entry.finished}</th> <th>${entry.days}</th> <th>${entry.rating}</th><th><button onclick="removeRow(\'${entry.title}\')">delete</button></th></tr>`
+  })
 }
+
+window.onload = function () {
+  const button = document.querySelector("button");
+  button.onclick = submit
+  fetch('/data', {
+    method: 'GET',
+  }).then(response => response.json())
+    .then(json => {
+      updateTable(json)
+    })
+}
+
--- server.improved.js
+++ server.improved.js
@@ -1,74 +1,118 @@
-const http = require( 'http' ),
-      fs   = require( 'fs' ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library if you're testing this on your local machine.
-      // However, Glitch will install it automatically by looking in your package.json
-      // file.
-      mime = require( 'mime' ),
-      dir  = 'public/',
-      port = 3000
+const http = require('http'),
+  fs = require('fs'),
+  mime = require('mime'),
+  dir = 'public/',
+  port = 3000
 
-const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
+let appdata = [
+  {
+    'title': 'bookTitle',
+    'author': 'bookAuthor',
+    'started': '2023-01-01',
+    'finished': '2023-02-01',
+    'days': '31',
+    'rating': 10,
+  },
 ]
 
-const server = http.createServer( function( request,response ) {
-  if( request.method === 'GET' ) {
-    handleGet( request, response )    
-  }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
+const server = http.createServer(function (request, response) {
+  if (request.method === 'GET') {
+    handleGet(request, response)
+  } else if (request.method === 'POST') {
+    handlePost(request, response)
+  }
+  else if (request.method === 'DELETE') {
+    handleDelete(request, response)
   }
 })
+const handleGet = function (request, response) {
+  const filename = dir + request.url.slice(1)
+  if (request.url === '/') {
+    sendFile(response, 'public/index.html')
+  }
+  else if (request.url === '/data') {
+    console.log(JSON.stringify(appdata))
+    response.writeHeader(200, { "Content-type": "text/json" });
+    response.end(JSON.stringify(appdata));
+  }
+  else {
+    sendFile(response, filename)
+  }
+}
+const handlePost = function (request, response) {
+  console.log("request URL" + request.url);
+  let dataString = ''
+  request.on('data', function (data) {
+    dataString += data
+  })
+  request.on('end', function () {
+    let postResponse = JSON.parse(dataString);
 
-const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
+    let days = daysTaken(postResponse.started, postResponse.finished)
 
-  if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
-  }else{
-    sendFile( response, filename )
+    appdata.push({ title: postResponse.title, author: postResponse.author, started: postResponse.started, finished: postResponse.finished, days: days, rating: postResponse.rating })
+    response.writeHead(200, "OK", { 'Content-Type': 'text/json' })
+    response.end(JSON.stringify(appdata))
+  })
+}
+
+const handleDelete = function (request, response) {
+  let dataString = ''
+  request.on('data', function (data) {
+    dataString += data
+  })
+  request.on('end', function () {
+    let titleToRemove = JSON.parse(dataString).titleToRemove
+    appdata = appdata.filter(function (n, i) {
+      return n.title !== titleToRemove;
+    })
+    response.writeHead(200, "OK", { 'Content-Type': 'text/plain' })
+    response.end(JSON.stringify(appdata))
+  })
+}
+
+const sendFile = function (response, filename) {
+  const type = mime.getType(filename)
+
+  fs.readFile(filename, function (err, content) {
+
+    // if the error = null, then we've loaded the file successfully
+    if (err === null) {
+
+      // status code: https://httpstatuses.com
+      response.writeHeader(200, { 'Content-Type': type })
+      response.end(content)
+
+    } else {
+
+      // file not found, error code 404
+      response.writeHeader(404)
+      response.end('404 Error: File Not Found')
+
+    }
+  })
+}
+
+const daysTaken = (startDate, endDate) => {
+  console.log(startDate + endDate)
+  let start = getDate(startDate);
+  let end = getDate(endDate);
+
+  if (start.getDate() && end.getDate()) {
+    if (start.getTime() > end.getTime()) {
+      console.log("Error, start must be before end.")
+      return -1;
+    }
+    let timeDifference = start.getTime() - end.getTime();
+    let dayDifference = Math.abs(timeDifference / (1000 * 3600 * 24));
+    console.log(dayDifference)
+    return dayDifference;
   }
 }
 
-const handlePost = function( request, response ) {
-  let dataString = ''
-
-  request.on( 'data', function( data ) {
-      dataString += data 
-  })
-
-  request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
-
-    // ... do something with the data here!!!
-
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end('test')
-  })
+function getDate(stringDate) {
+  var yearMonthDate = stringDate.split('-');
+  return new Date(yearMonthDate[0], yearMonthDate[1], yearMonthDate[2]);
 }
 
-const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
-
-   fs.readFile( filename, function( err, content ) {
-
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
-
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
-
-     }else{
-
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
-
-     }
-   })
-}
-
-server.listen( process.env.PORT || port )
+server.listen(process.env.PORT || port)
