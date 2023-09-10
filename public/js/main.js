a99af5941273af38a1f945ea575deedd0962a511
MarekSGarbaczonek <msgarbaczonek@gmail.com>
2023-09-10 03:52:15 -0400

    Add Task

--- public/js/main.js
+++ public/js/main.js
@@ -22,6 +22,44 @@ const submit = async function( event ) {
 }
 
 window.onload = function() {
-   const button = document.querySelector("button");
+  const button = document.querySelector("button");
   button.onclick = submit;
+}
+
+
+function updateTaskRows() {
+  const table = document.getElementById("task-table");
+
+  for (let i = 1; i <= 5; i++) {
+    const newRow = document.createElement("tr");
+    const taskCell = document.createElement("td");
+
+    const leftContainer = document.createElement("div");
+    leftContainer.className = "left-container";
+
+    const doneButton = document.createElement("button");
+    doneButton.className = "Completion-Button";
+
+    const taskText = document.createTextNode(`Task ${i}`);
+
+    const rightContainer = document.createElement("div");
+    rightContainer.className = "right-container";
+
+    const myDayButton = document.createElement("button");
+    myDayButton.className = "MyDay-Button";
+
+    leftContainer.appendChild(doneButton);
+    leftContainer.appendChild(taskText);
+    rightContainer.appendChild(myDayButton);
+
+    taskCell.appendChild(leftContainer);
+    taskCell.appendChild(rightContainer);
+
+    newRow.appendChild(taskCell);
+    table.appendChild(newRow);
+  }
+}
+
+window.onload = function (){
+  updateTaskRows();
 }
