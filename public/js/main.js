188d5cb9db45c9b16df1616e639d6292222572c4
apblanchard4 <107159438+apblanchard4@users.noreply.github.com>
2023-09-10 21:42:48 -0400

    Final updates

--- public/js/main.js
+++ public/js/main.js
@@ -29,6 +29,7 @@ const submit = async function (event) {
   updateTable(data);
 }
 const removeRow = async function (bookTitle) {
+
   let json = { titleToRemove: bookTitle }
   let body = JSON.stringify(json)
   fetch('/remove', {
@@ -45,11 +46,11 @@ const removeRow = async function (bookTitle) {
 const updateTable = function (jsonData) {
   const table = document.querySelector("table")
   console.log(table)
-  table.innerHTML = "<tr> <th>Title</th> <th>Author</th> <th>Started</th><th>Finished</th><th>Days taken to Read</th><th>Rating</th><th>Delete</th></tr>"
+  table.innerHTML = "<tr> <th>Title</th> <th>Author</th> <th>Started</th><th>Finished</th><th>Days to Read</th><th>Rating</th><th>Delete</th></tr>"
 
 
   jsonData.forEach(entry => {
-    table.innerHTML += `<tr> <th>${entry.title}</th> <th>${entry.author}</th> <th>${entry.started}</th> <th>${entry.finished}</th> <th>${entry.days}</th> <th>${entry.rating}</th><th><button onclick="removeRow(\'${entry.title}\')">delete</button></th></tr>`
+    table.innerHTML += `<tr> <td>${entry.title}</td> <td>${entry.author}</td> <td>${entry.started}</td> <td>${entry.finished}</td> <td>${entry.days}</td> <td>${entry.rating}</td><td><button onclick="removeRow(\'${entry.title}\')">X</button></td></tr>`
   })
 }
 
