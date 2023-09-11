c0423d964f3006b516c0f58754399233565c6a18
AarshZadaphiya <aszadaphiya@wpi.edu>
2023-09-11 16:54:03 -0400

    derived field added

--- public/js/main.js
+++ public/js/main.js
@@ -59,10 +59,16 @@ for (const key in data) {
       row.appendChild( cell )
     }
     
-    
+    const dueDate = new Date(data[key]["date"]);
+        const currentDate = new Date();
+        const daysRemaining = Math.ceil((dueDate - currentDate) / (1000 * 60 * 60 * 24));
+
+        const daysRemainingCell = document.createElement("td");
+        daysRemainingCell.innerText = daysRemaining > 0 ? `${daysRemaining} days` : "Expired";
+        row.appendChild(daysRemainingCell);
     
     const deleteButton = document.createElement('button')
-    deleteButton.innerText = 'Delete Item';
+    deleteButton.innerText = 'Delete';
     deleteButton.className = 'my-deletebutton';
     // store current value of i 
     let _i = i
@@ -72,12 +78,23 @@ for (const key in data) {
     deleteCell.appendChild(deleteButton);
     row.appendChild(deleteCell);
 
-
+    /*resultListHTML += '<tr>';
+    resultListHTML += `<td>${data[key]['tasks']}</td>`;
+    resultListHTML += `<td>${data[key]['date']}</td>`;
+    resultListHTML += `<td>${data[key]['priority']}</td>`;
+    resultListHTML += `<td> <button class="my-deletebutton" onclick="window.delete(${i})" type="button" >Delete Item</button></td>`;
+    resultListHTML += '</tr>';*/
+    
     i++
     tableparse.appendChild( row )
     //console.log(data)
   }
 }
+//tableparse.innerHTML= resultListHTML;
+
+  /*const resultContainer = document.querySelector('#result');
+  resultContainer.innerHTML = resultListHTML;
+  resultContainer.appendChild(list);*/
       })
       
   }
