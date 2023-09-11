e20ad791d2f7855cb58caaf0973f1bfc3a71eefd
Glitch (a2-wduf) <none>
2023-09-11 02:26:43 +0000

    📄🚐 Updated with Glitch

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,149 @@
 // FRONT-END (CLIENT) JAVASCRIPT HERE
 
-const submit = async function( event ) {
-  // stop form submission from trying to load
-  // a new .html page for displaying results...
-  // this was the original browser behavior and still
-  // remains to this day
-  event.preventDefault()
+function validate() {
+  /*
+  Validate that all inputs are whole numbers. Anything else sets that input to 0.
+  */
+  let input_nodes = document.querySelectorAll("input");
+  for (let input of input_nodes) {
+    let value = input.value;
+    let illegal_flag = false;
+    if((value[0] < "0" || value[0] > "9") && value[0] != "-") {
+      illegal_flag = true;
+    }
+    for (let i = 1; i < value.length; i++) {
+      let c = value[i];
+      if (c < "0" || c > "9") {
+        illegal_flag = true;
+      }
+    }
+    if (illegal_flag) {
+      input.value = "0";
+    }
+  }
+}
+
+function reset() {
+  let input_nodes = document.querySelectorAll("input");
+  for (let input of input_nodes) {
+    input.value = "0";
+  }
+}
+
+async function submit(event) {
+  // Stop displaying results in new page by default.
+  event.preventDefault();
+
+  // Get inputs elements.
+  let input_nodes = document.querySelectorAll('input');
   
-  const input = document.querySelector( '#yourname' ),
-        json = { yourname: input.value },
-        body = JSON.stringify( json )
+  // Create variable to store input values in JSON format.
+  let data = {}
+  
+  // Fill json with input values.
+  for(let input of input_nodes) {
+    data[input.id] = parseInt(input.value);
+  }
 
-  const response = await fetch( '/submit', {
-    method:'POST',
-    body 
-  })
-
-  const text = await response.text()
-
-  console.log( 'text:', text )
+  // Send POST request to server.
+  let response = await fetch("/submit", {
+    'method': "POST",
+    'headers': {
+      "Content-Type": "application/json"
+    },
+    'body': JSON.stringify(data)
+  });
+  
+  // Get response in JSON format.
+  let result = await response.json();
+  
+  // Return the response.
+  return result;
 }
 
-window.onload = function() {
-   const button = document.querySelector("button");
-  button.onclick = submit;
+async function double(event) {
+  // Stop displaying results in new page by default.
+  event.preventDefault();
+
+  // Send POST request to server.
+  let response = await fetch("/double", {
+    'method': "POST",
+    'headers': {
+      "Content-Type": "application/json"
+    },
+    'body': JSON.stringify({})
+  });
+  
+  // Get response in JSON format.
+  let result = await response.json();
+  
+  // Return the response.
+  return result;
 }
+
+async function clear(event) {
+  // Stop displaying results in new page by default.
+  event.preventDefault();
+
+  // Send POST request to server.
+  let response = await fetch("/clear", {
+    'method': "POST",
+    'headers': {
+      "Content-Type": "application/json"
+    },
+    'body': JSON.stringify({})
+  });
+  
+  // Get response in JSON format.
+  let result = await response.json();
+  
+  // Return the response.
+  return result;
+}
+
+function redraw(data) {
+  // Select table and get current row count.
+  let table = document.querySelector("table");
+  let row_count = table.rows.length;
+  
+  // Delete all current rows except header row.
+  for(let i = row_count - 1; i > 0; i--) {
+    table.deleteRow(i)
+  }
+  
+  // Add the response data to the table.
+  for(let i = 0; i < data.length; i++) {
+    let current_data = data[i]
+    let row = table.insertRow(i + 1);
+    row.insertCell(0).innerHTML = current_data['number1']
+    row.insertCell(1).innerHTML = current_data['number2']
+    row.insertCell(2).innerHTML = current_data['number3']
+    row.insertCell(3).innerHTML = current_data['sum']
+  }
+}
+
+window.onload = async function () {
+  // Add submit button behavior.  
+  let submit_button = document.querySelector("#submit-button");
+  submit_button.onclick = async (event) => {
+    validate();
+    redraw(await submit(event));
+    reset();
+  };
+  
+  // Add clear button behavior.  
+  let clear_button = document.querySelector("#clear-button");
+  clear_button.onclick = async (event) => {
+    redraw(await clear(event));
+  };
+  
+  // Add double button behavior.  
+  let double_button = document.querySelector("#double-button");
+  double_button.onclick = async (event) => {
+    redraw(await double(event));
+  };
+  
+  // Fetch the current data from the server and display it.
+  validate();
+  redraw(await submit(event));
+};
--- server.improved.js
+++ server.improved.js
@@ -1,74 +1,98 @@
-const http = require( 'http' ),
-      fs   = require( 'fs' ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library if you're testing this on your local machine.
-      // However, Glitch will install it automatically by looking in your package.json
-      // file.
-      mime = require( 'mime' ),
-      dir  = 'public/',
-      port = 3000
+// IMPORTANT: you must run `npm install` in the directory for this assignment
+// to install the mime library if you're testing this on your local machine.
+// However, Glitch will install it automatically by looking in your package.json
+// file.
 
-const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
-]
+const http = require("http"),
+  fs = require("fs"),
+  mime = require("mime"),
+  dir = "public/",
+  port = 3000;
 
-const server = http.createServer( function( request,response ) {
-  if( request.method === 'GET' ) {
-    handleGet( request, response )    
-  }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
+const sendFile = function (response, filename) {
+  const type = mime.getType(filename);
+  fs.readFile(filename, function (err, content) {
+    // if the error = null, then we've loaded the file successfully
+    if (err === null) {
+      // status code: https://httpstatuses.com
+      response.writeHeader(200, { "Content-Type": type });
+      response.end(content);
+    } else {
+      // file not found, error code 404
+      response.writeHeader(404);
+      response.end("404 Error: File Not Found");
+    }
+  });
+};
+
+const server = http.createServer((request, response) => {
+  if (request.method === "GET") {
+    handleGet(request, response);
+  } else if (request.method === "POST") {
+    handlePost(request, response);
   }
-})
+});
 
-const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
-
-  if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
-  }else{
-    sendFile( response, filename )
+const handleGet = (request, response) => {
+  const filename = dir + request.url.slice(1);
+  if (request.url === "/") {
+    sendFile(response, "public/index.html");
+  } else {
+    sendFile(response, filename);
   }
-}
+};
 
-const handlePost = function( request, response ) {
-  let dataString = ''
+// DON'T TOUCH ANYTHING ABOVE THIS.
 
-  request.on( 'data', function( data ) {
-      dataString += data 
-  })
+let app_data = [{ number1: 1, number2: 2, number3: 3, sum: 6 }];
 
-  request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
+const handlePost = (request, response) => {
+  // Get data from POST request.
+  let data_string = "";
+  request.on("data", (data) => {
+    data_string += data;
+  });
 
-    // ... do something with the data here!!!
+  // Do something with that data.
+  request.on("end", () => {
+    // Parse the request data.
+    let json = JSON.parse(data_string);
+    
+    // Handle different functions separately.
+    switch(request.url) {
+      case "/submit": 
+        // Add new data.
+        app_data.push({
+          number1: json.number1,
+          number2: json.number2,
+          number3: json.number3,
+          sum: json.number1 + json.number2 + json.number3,
+        });
+        break;
+      case "/clear": 
+        // Clear all app_data.
+        app_data = [];
+        break;
+      case "/double": 
+        // Double all app_data.
+        for(let i = 0; i < app_data.length; i++) {
+          let row = app_data[i];
+          app_data[i] = {
+            "number1": row["number1"] * 2,
+            "number2": row["number2"] * 2,
+            "number3": row["number3"] * 2,
+            "sum": row["sum"] * 2
+          };
+        }
+        break;
+      default:
+        console.log("ERROR: Unknown request function type.");
+    }
 
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end('test')
-  })
-}
+    // Send back response with app_data.
+    response.writeHead(200, "OK", { "Content-Type": "application/json" });
+    response.end(JSON.stringify(app_data));
+  });
+};
 
-const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
-
-   fs.readFile( filename, function( err, content ) {
-
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
-
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
-
-     }else{
-
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
-
-     }
-   })
-}
-
-server.listen( process.env.PORT || port )
+server.listen(process.env.PORT || port);
