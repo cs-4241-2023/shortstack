e73f3cbed0b7523b63986bfa77c24f922a666412
Andrew Hariyanto <51850304+andrewhariyanto@users.noreply.github.com>
2023-09-11 19:57:09 -0400

    final submission

--- public/js/main.js
+++ public/js/main.js
@@ -1,70 +1,302 @@
 // FRONT-END (CLIENT) JAVASCRIPT HERE
 
 const submit = async function (event) {
-  console.log('passed3');
   // stop form submission from trying to load
   // a new .html page for displaying results...
   // this was the original browser behavior and still
   // remains to this day
-  event.preventDefault()
+  event.preventDefault();
 
-  const keyInput = document.querySelector('#key-input'),
-    startInput = document.querySelector('#start-date-input'),
-    endInput = document.querySelector('#end-date-input'),
-    json = { key: keyInput.value, startDate: startInput.value, endDate: endInput.value },
+  const depostInput = document.querySelector("#deposit-input"),
+    startInput = document.querySelector("#start-date-input"),
+    rateInput = document.querySelector("#rate-input"),
+    json = {
+      deposit: depostInput.value / 1,
+      startDate: startInput.value,
+      rate: rateInput.value / 100,
+    },
     body = JSON.stringify(json);
 
-  console.log('passed2');
-  const response = await fetch('/submit', {
-    method: 'POST',
-    body
-  })
-
-  const data = await response.json()
-
-  console.log('text:', data)
-  displayResults(data);
-}
-
-const firstLoad = async function () {
-  const response = await fetch("/getData");
+  console.log(body);
+  const response = await fetch("/submit", {
+    method: "POST",
+    body,
+  });
 
   const data = await response.json();
 
-  displayResults(data);
-}
+  console.log("text:", data);
+  update(data);
+  hideDeleteButtons();
+};
 
-const displayResults = (data) => {
-  console.log(data.map(d => d.key));
-  document.getElementById('data').innerHTML = '';
+const firstLoad = async function () {
+  const response = await fetch("/getData");
+  
+  console.log("first load");
+
+  const data = await response.json();
+
+  update(data);
+  hideDeleteButtons();
+};
+
+const hideModifyView = () => {
+  document.getElementById("modifyView").hidden = true;
+  document.getElementById("dataView").hidden = false;
+
+  document.getElementById("finishModifyButton").hidden = true;
+  document.getElementById("modifyButton").hidden = false;
+  document.getElementById("deleteButton").hidden = false;
+};
+
+const showModifyView = () => {
+  document.getElementById("modifyView").hidden = false;
+  document.getElementById("dataView").hidden = true;
+
+  document.getElementById("finishModifyButton").hidden = false;
+  document.getElementById("modifyButton").hidden = true;
+  document.getElementById("deleteButton").hidden = true;
+};
+
+const displayModifyView = (data) => {
+  document.getElementById("modifyView").innerHTML = "";
 
   for (let i = 0; i < data.length; i++) {
-    let tableRow = document.createElement('tr');
-    let keyColumn = document.createElement('td'),
-      startDateColumn = document.createElement('td'),
-      endDateColumn = document.createElement('td'),
-      dueDateColumn = document.createElement('td');
+    let tableRow = document.createElement("tr"),
+      idColumn = document.createElement("td"),
+      depositColumn = document.createElement("td"),
+      startDateColumn = document.createElement("td"),
+      rateColumn = document.createElement("td"),
+      accruedColumn = document.createElement("td"),
+      deleteButtonColumn = document.createElement("td");
 
-    let textKey = document.createTextNode(data[i].key),
-      textStart = document.createTextNode(data[i].startDate),
-      textEnd = document.createTextNode(data[i].endDate),
-      start = new Date(data[i].startDate),
-      end = new Date(data[i].endDate),
-      due = end.getTime() - start.getTime(),
-      textDiff = document.createTextNode(due / (1000 * 60 * 60 * 24));
-    keyColumn.appendChild(textKey);
-    startDateColumn.appendChild(textStart);
-    endDateColumn.appendChild(textEnd);
-    dueDateColumn.appendChild(textDiff);
-    tableRow.appendChild(keyColumn);
+    //input elements
+    let textId = document.createElement("p"),
+      inputDeposit = document.createElement("input"),
+      inputStart = document.createElement("input"),
+      inputRate = document.createElement("input"),
+      textAccrued = document.createElement("p");
+
+    inputDeposit.setAttribute("type", "number");
+    inputDeposit.setAttribute("min", "0");
+    inputDeposit.setAttribute("step", "0.01");
+    inputDeposit.setAttribute("form", "modifyForm");
+
+    inputRate.setAttribute("type", "number");
+    inputRate.setAttribute("min", "0");
+    inputRate.setAttribute("max", "100");
+    inputRate.setAttribute("step", "0.01");
+    inputRate.setAttribute("form", "modifyForm");
+
+    inputStart.setAttribute("type", "date");
+    inputStart.setAttribute("form", "modifyForm");
+    setTodayMax(inputStart);
+
+    textId.textContent = data[i].id;
+    inputDeposit.value = data[i].deposit;
+    inputStart.value = data[i].startDate;
+    inputRate.value = data[i].rate * 100;
+
+    idColumn.appendChild(textId);
+    idColumn.hidden = true;
+    depositColumn.appendChild(inputDeposit);
+    startDateColumn.appendChild(inputStart);
+    rateColumn.appendChild(inputRate);
+    accruedColumn.appendChild(textAccrued);
+    tableRow.appendChild(idColumn);
+    tableRow.appendChild(depositColumn);
     tableRow.appendChild(startDateColumn);
-    tableRow.appendChild(endDateColumn);
-    tableRow.appendChild(dueDateColumn);
-    document.querySelector('#data').appendChild(tableRow);
+    tableRow.appendChild(rateColumn);
+    tableRow.appendChild(accruedColumn);
+
+    document.querySelector("#modifyView").appendChild(tableRow);
   }
-}
+};
+
+const displayResults = (data) => {
+  document.getElementById("dataView").innerHTML = "";
+
+  for (let i = 0; i < data.length; i++) {
+    let tableRow = document.createElement("tr");
+    let idColumn = document.createElement("td"),
+      depositColumn = document.createElement("td"),
+      startDateColumn = document.createElement("td"),
+      rateColumn = document.createElement("td"),
+      accruedColumn = document.createElement("td"),
+      deleteButtonColumn = document.createElement("td");
+
+    let textId = document.createElement("p"),
+      textDeposit = document.createElement("p"),
+      textStart = document.createElement("p"),
+      textRate = document.createElement("p"),
+      textAccrued = document.createElement("p"),
+      deleteButton = document.createElement("button");
+
+    textId.textContent = data[i].id;
+    textDeposit.textContent = data[i].deposit.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+    textStart.textContent = data[i].startDate;
+    textRate.textContent = data[i].rate * 100 + "%";
+    textAccrued.textContent = data[i].accrued.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+    idColumn.appendChild(textId);
+    idColumn.hidden = true;
+    depositColumn.appendChild(textDeposit);
+    startDateColumn.appendChild(textStart);
+    rateColumn.appendChild(textRate);
+    accruedColumn.appendChild(textAccrued);
+    tableRow.appendChild(idColumn);
+    tableRow.appendChild(depositColumn);
+    tableRow.appendChild(startDateColumn);
+    tableRow.appendChild(rateColumn);
+    tableRow.appendChild(accruedColumn);
+
+    //button stuff
+    deleteButton.textContent = "Delete";
+    deleteButton.onclick = async () => {
+      await deleteData(i);
+      //document.getElementById("dataView").deleteRow(i);
+    };
+    deleteButtonColumn.appendChild(deleteButton);
+    deleteButtonColumn.setAttribute("class", "deleteButton");
+    deleteButtonColumn.hidden = true;
+    tableRow.appendChild(deleteButtonColumn);
+
+    document.querySelector("#dataView").appendChild(tableRow);
+  }
+};
+
+const showDeleteButtons = function () {
+  document.getElementById("deleteColumn").hidden = false;
+
+  let buttons = document.querySelectorAll(".deleteButton");
+
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].hidden = false;
+  }
+
+  document.querySelector("#deleteButton").hidden = true;
+  document.querySelector("#finishDeleteButton").hidden = false;
+
+  document.getElementById("modifyButton").hidden = true;
+};
+
+const hideDeleteButtons = function () {
+  document.getElementById("deleteColumn").hidden = true;
+  document.querySelector("#finishDeleteButton").hidden = true;
+  let buttons = document.querySelectorAll(".deleteButton");
+
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].hidden = true;
+  }
+};
+
+const deleteData = async function (row) {
+  let dataRow = document.getElementById("dataView").getElementsByTagName("tr");
+  let idValue = dataRow[row].querySelector("td"),
+    json = {
+      id: Number(idValue.textContent),
+    },
+    body = JSON.stringify(json);
+
+  const response = await fetch("/delete", {
+    method: "POST",
+    body,
+  });
+
+  const data = await response.json();
+
+  console.log("delete:", data);
+  update(data);
+  showDeleteButtons();
+};
+
+const showController = function () {
+  //show all controller buttons
+  document.querySelector("#deleteButton").hidden = false;
+  document.querySelector("#modifyButton").hidden = false;
+
+  //hide all delete buttons
+  hideDeleteButtons();
+
+  //hide finish button
+  document.querySelector("#finishDeleteButton").hidden = true;
+  document.querySelector("#finishModifyButton").hidden = true;
+};
+
+const setTodayMax = function (element) {
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth() + 1;
+  var yyyy = today.getFullYear();
+
+  if (dd < 10) {
+    dd = "0" + dd;
+  }
+
+  if (mm < 10) {
+    mm = "0" + mm;
+  }
+
+  today = yyyy + "-" + mm + "-" + dd;
+  element.setAttribute("max", today);
+};
+
+const sendModifiedReq = async (event) => {
+  event.preventDefault();
+
+  const modifiedData = [];
+
+  let modifiedDataRow = document.getElementById("modifyView").children;
+  for (let i = 0; i < modifiedDataRow.length; i++) {
+    let inputs = modifiedDataRow[i].children;
+
+    let id = inputs[0].firstElementChild,
+      depositInput = inputs[1].firstElementChild,
+      startInput = inputs[2].firstElementChild,
+      rateInput = inputs[3].firstElementChild;
+
+    let json = {
+      deposit: depositInput.value / 1,
+      startDate: startInput.value,
+      rate: rateInput.value / 100,
+      id: id.textContent,
+    };
+
+    modifiedData.push(json);
+  }
+
+  let body = JSON.stringify(modifiedData);
+
+  const response = await fetch("/modify", {
+    method: "POST",
+    body,
+  });
+
+  const data = await response.json();
+
+  update(data);
+  hideModifyView();
+};
+
+const update = (data) => {
+  displayResults(data);
+  displayModifyView(data);
+  hideModifyView();
+};
 
 window.onload = function () {
-  document.querySelector('#taskForm').onsubmit = submit;
+  document.querySelector("#taskForm").onsubmit = submit;
+  document.querySelector("#deleteButton").onclick = showDeleteButtons;
+  document.querySelector("#finishDeleteButton").onclick = showController;
+  document.querySelector("#modifyButton").onclick = showModifyView;
+  document.querySelector("#modifyForm").onsubmit = sendModifiedReq;
+  console.log("loaded");
+  setTodayMax(document.getElementById("start-date-input"));
   firstLoad();
-}
+};
--- server.improved.js
+++ server.improved.js
@@ -1,80 +1,171 @@
-const http = require('http'),
-  fs = require('fs'),
+const http = require("http"),
+  fs = require("fs"),
   // IMPORTANT: you must run `npm install` in the directory for this assignment
   // to install the mime library if you're testing this on your local machine.
   // However, Glitch will install it automatically by looking in your package.json
   // file.
-  mime = require('mime'),
-  dir = 'public/',
-  port = 3000
+  mime = require("mime"),
+  dir = "public/",
+  port = 3000;
 
 const appdata = [
-  { key: 'MQP-1', startDate: '2023-09-07', endDate: '2023-09-23' },
-  { key: 'MQP-2', startDate: '2023-09-07', endDate: '2023-09-17' },
-  { key: 'MQP-3', startDate: '2023-09-07', endDate: '2023-09-20' },
+  {
+    deposit: 1000.0,
+    startDate: "2022-09-07",
+    rate: 0.04,
+    accrued: 1040.0,
+    id: 1,
+  },
+  {
+    deposit: 2000.0,
+    startDate: "2021-09-04",
+    rate: 0.04,
+    accrued: 2163.23,
+    id: 2,
+  },
+  {
+    deposit: 3500.0,
+    startDate: "2020-10-04",
+    rate: 0.05,
+    accrued: 3503.499,
+    id: 3,
+  },
 ];
 
 const server = http.createServer(function (request, response) {
-  if (request.method === 'GET') {
-    handleGet(request, response)
-  } else if (request.method === 'POST') {
-    console.log('passed1')
-    handlePost(request, response)
+  if (request.method === "GET") {
+    handleGet(request, response);
+  } else if (request.method === "POST") {
+    handlePost(request, response);
   }
-})
+});
 
 const handleGet = function (request, response) {
-  const filename = dir + request.url.slice(1)
+  const filename = dir + request.url.slice(1);
 
-  if (request.url === '/') {
-    sendFile(response, 'public/index.html')
+  if (request.url === "/") {
+    sendFile(response, "public/index.html");
   } else if (request.url === "/getData") {
     const data = JSON.stringify(appdata);
+    console.log("first load get data");
     response.writeHeader(200, { "Content-Type": "application/json" });
     response.end(data);
   } else {
-    sendFile(response, filename)
+    sendFile(response, filename);
   }
-}
+};
 
 const handlePost = function (request, response) {
-  let dataString = ''
+  switch (request.url) {
+    case "/submit":
+      let dataString = "";
 
-  request.on('data', function (data) {
-    dataString += data
-  })
+      request.on("data", function (data) {
+        dataString += data;
+        console.log(dataString);
+      });
 
-  request.on('end', function () {
-    console.log(JSON.parse(dataString))
-    appdata.push(JSON.parse(dataString));
+      request.on("end", function () {
+        console.log(JSON.parse(dataString));
 
-    // ... do something with the data here!!
+        let data = JSON.parse(dataString),
+          start = new Date(data.startDate),
+          today = new Date(),
+          diff = new Date(today - start).getFullYear() - 1970,
+          deposit = data.deposit,
+          rate = data.rate,
+          accrued = deposit * (1 + rate * diff);
 
-    response.writeHeader(200, "OK", { 'Content-Type': 'application/json' })
-    response.end(JSON.stringify(appdata))
-  })
-}
+        data["accrued"] = accrued;
+        data["id"] = Number(Date.now());
+
+        appdata.push(data);
+
+        response.writeHeader(200, "OK", { "Content-Type": "application/json" });
+        console.log(JSON.stringify(appdata));
+        response.end(JSON.stringify(appdata));
+      });
+      break;
+    case "/delete":
+      let idValue = "";
+
+      request.on("data", function (data) {
+        idValue += data;
+      });
+
+      request.on("end", function () {
+        console.log(JSON.parse(idValue));
+
+        let obj = JSON.parse(idValue);
+
+        for (let i = 0; i < appdata.length; i++) {
+          if (equalObject(obj, appdata[i])) {
+            appdata.splice(i, 1);
+            break;
+          }
+        }
+
+        console.log(appdata);
+        response.writeHeader(200, "OK", { "Content-Type": "application/json" });
+        response.end(JSON.stringify(appdata));
+      });
+
+      break;
+    case "/modify":
+      let modifiedData = "";
+
+      request.on("data", function (data) {
+        modifiedData += data;
+        console.log(modifiedData);
+      });
+
+      request.on("end", function () {
+        console.log(JSON.parse(modifiedData));
+
+        let newData = JSON.parse(modifiedData);
+        for (let i = 0; i < newData.length; i++) {
+          let start = new Date(newData[i].startDate),
+            today = new Date(),
+            diff = new Date(today - start).getFullYear() - 1970,
+            deposit = newData[i].deposit,
+            rate = newData[i].rate,
+            accrued = deposit * (1 + rate * diff);
+
+          newData[i]["accrued"] = accrued;
+          newData[i]["id"] = Number(newData[i]["id"]);
+        }
+
+        appdata.splice(0, newData.length, ...newData);
+
+        response.writeHeader(200, "OK", { "Content-Type": "application/json" });
+        console.log(JSON.stringify(appdata));
+        response.end(JSON.stringify(appdata));
+      });
+      break;
+    default:
+      break;
+  }
+};
+
+const equalObject = function (obj1, obj2) {
+  return obj1.id === obj2.id;
+};
 
 const sendFile = function (response, filename) {
-  const type = mime.getType(filename)
+  const type = mime.getType(filename);
 
   fs.readFile(filename, function (err, content) {
-
     // if the error = null, then we've loaded the file successfully
     if (err === null) {
-
       // status code: https://httpstatuses.com
-      response.writeHeader(200, { 'Content-Type': type })
-      response.end(content)
-
+      response.writeHeader(200, { "Content-Type": type });
+      response.end(content);
     } else {
-
       // file not found, error code 404
-      response.writeHeader(404)
-      response.end('404 Error: File Not Found')
-
+      response.writeHeader(404);
+      response.end("404 Error: File Not Found");
     }
-  })
-}
+  });
+};
 
-server.listen(process.env.PORT || port)
+server.listen(process.env.PORT || port);
