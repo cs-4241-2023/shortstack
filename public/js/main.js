566a8bc8bcbb972c103376739566503195355771
Mahir Sowad <32722691+mahirsowad3@users.noreply.github.com>
2023-09-11 17:47:56 -0400

    derived fields appear in table

--- public/js/main.js
+++ public/js/main.js
@@ -34,6 +34,7 @@ const submit = async function (event) {
   const text = await response.text();
 
   tasks = JSON.parse(text);
+  updateTaskTable(tasks);
   console.log("new tasks list: ", tasks);
 
   // const input = document.querySelector("#yourname"),
@@ -52,6 +53,43 @@ const submit = async function (event) {
 
 const updateTaskTable = function (tasks) {
   const table = document.getElementById("tasksTable");
+  const tasksTableRowsContainer = document.getElementsByClassName(
+    "tasks-table-rows-container"
+  );
+  for (let i = 0; i < tasks.length; i++) {
+    const currentTask = tasks[i];
+    // create a new cell for each field
+    const taskNameCell = document.createElement("td");
+    const taskDescriptionCell = document.createElement("td");
+    const taskCreatedCell = document.createElement("td");
+    const taskDeadlineCell = document.createElement("td");
+    const taskPriorityCell = document.createElement("td");
+    const taskTotalTimeCell = document.createElement("td");
+    const taskTimeRemainingCell = document.createElement("td");
+    // add the text to each cell
+    taskNameCell.innerText = currentTask.taskName;
+    taskDescriptionCell.innerText = currentTask.taskDescription;
+    taskCreatedCell.innerText = currentTask.taskCreated;
+    taskDeadlineCell.innerText = currentTask.taskDeadline;
+    taskPriorityCell.innerText = currentTask.taskPriority;
+    taskTotalTimeCell.innerText = currentTask.totalTime;
+    if (currentTask.timeRemaining < 0) {
+      taskTimeRemainingCell.innerText = "OVERDUE";
+    } else {
+      taskTimeRemainingCell.innerText = currentTask.timeRemaining;
+    }
+    // append each cell to the row
+    const currentRow = document.createElement("tr");
+    currentRow.appendChild(taskNameCell);
+    currentRow.appendChild(taskDescriptionCell);
+    currentRow.appendChild(taskCreatedCell);
+    currentRow.appendChild(taskDeadlineCell);
+    currentRow.appendChild(taskPriorityCell);
+    currentRow.appendChild(taskTotalTimeCell);
+    currentRow.appendChild(taskTimeRemainingCell);
+    // append the row to the table
+    table.appendChild(currentRow);
+  }
 };
 
 window.onload = function () {
