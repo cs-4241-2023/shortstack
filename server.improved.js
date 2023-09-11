1cd9a92b151a37ebb4656d33657ee1ef57e8daf3
Nicholas Borrello <nvborrello@gmail.com>
2023-09-11 16:18:26 -0400

    everything

--- server.improved.js
+++ server.improved.js
@@ -9,9 +9,6 @@ const http = require( 'http' ),
       port = 3000
 
 const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
 ]
 
 const server = http.createServer( function( request,response ) {
