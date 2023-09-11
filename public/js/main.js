f032e4b19caf856c7d8d7dc140bbf30141094a3b
vuongdbrandon <57996169+bdvuong@users.noreply.github.com>
2023-09-10 22:13:02 -0400

    Implement most functionality

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
 // FRONT-END (CLIENT) JAVASCRIPT HERE
-let taskData = []
+let taskData = [];
+let currentNote = 0;
 
 const submit = async function (event) {
   // stop form submission from trying to load
@@ -8,43 +9,101 @@ const submit = async function (event) {
   // remains to this day
   event.preventDefault();
 
-  const form = document.querySelector("form")
-  const body = parseForm(form)
+  const form = document.querySelector("form");
+  const body = parseForm(form);
 
   const response = await fetch("/submit", {
     method: "POST",
     body: JSON.stringify(body),
-  })
+  });
 
   const text = await response.text();
+  taskData = JSON.parse(text);
 
-  taskData = JSON.parse(text)
-  console.log(typeof(taskData))
-  console.log("taskData:", taskData)
+  form.reset();
+  loadTasks();
 };
 
-window.onload = function () {
+window.onload = async function () {
   const button = document.querySelector("#save");
   button.onclick = submit;
+
+  const response = await fetch("/init", {
+    method: "POST",
+    body: JSON.stringify("test"),
+  });
+
+  const text = await response.text();
+  taskData = JSON.parse(text);
+  loadTasks();
 };
 
 // collect form data and create an object that will be passed to the server
 function parseForm(formData) {
   let body = {};
-  Object.keys(formData).forEach(key => {
+  Object.keys(formData).forEach((key) => {
     let element = formData.elements[key];
-    if(element.type !== "submit" && element.type !== "button" && element.type !== "select") {
-      // console.log(element)
+    if (
+      element.type !== "submit" &&
+      element.type !== "button" &&
+      element.type !== "select"
+    ) {
       body[element.name] = element.value;
     } else if (element.type === "select") {
-      console.log(element)
+      console.log(element);
     }
-  })
+  });
   return body;
 }
 
-function newTask() {
-  const testButton = document.createElement('button')
-  testButton.textContent = "test"
-  document.querySelector("form").appendChild(testButton)
+function loadTasks() {
+  const sidebar = document.getElementById("sidebar");
+  sidebar.innerHTML = "";
+  const title = document.getElementById("task-title");
+  const date = document.getElementById("task-date");
+  const dueDate = document.getElementById("due-date");
+  const priority = document.getElementById("task-priority");
+  const description = document.getElementById("task-body");
+
+  taskData.forEach((task) => {
+    let newTask = document.createElement("div");
+
+    if (currentNote === 0) {
+      newTask.className = "tasks-list--task selected";
+      currentNote = task.id;
+      title.value = task.title;
+      date.value = task.date;
+      dueDate.value = task.dueDate;
+      priority.value = task.priority;
+      description.value = task.description;
+    } else {
+      newTask.className = "tasks-list--task";
+    }
+    newTask.innerText = task.title;
+    newTask.id = task.id;
+    newTask.addEventListener("click", async (event) => {
+      document.getElementById(currentNote).className = "tasks-list--task";
+      currentNote = event.target.id;
+      event.target.className = "tasks-list--task selected";
+      const taskID = await findTask(currentNote);
+      title.value = taskID.title;
+      date.value = taskID.date;
+      dueDate.value = taskID.dueDate;
+      priority.value = taskID.priority;
+      description.value = taskID.description;
+    });
+    sidebar.appendChild(newTask);
+  });
 }
+
+async function findTask(id) {
+  let returnTask = {};
+  taskData.forEach((task) => {
+    if (task.id === parseInt(id)) {
+      returnTask = task;
+    }
+  });
+  return returnTask;
+}
+
+function deleteNote() {}
--- server.improved.js
+++ server.improved.js
@@ -1,80 +1,120 @@
-const http = require( 'http' ),
-      fs   = require( 'fs' ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library if you're testing this on your local machine.
-      // However, Glitch will install it automatically by looking in your package.json
-      // file.
-      mime = require( 'mime' ),
-      dir  = 'public/',
-      port = 3000
+const http = require("http"),
+  fs = require("fs"),
+  // IMPORTANT: you must run `npm install` in the directory for this assignment
+  // to install the mime library if you're testing this on your local machine.
+  // However, Glitch will install it automatically by looking in your package.json
+  // file.
+  mime = require("mime"),
+  dir = "public/",
+  port = 3000;
 
 const appdata = [
+  {
+    title: "Assignment 1",
+    date: "2023-09-15",
+    priority: "Medium",
+    description: "Webware assignment 1",
+    dueDate: "2023-09-18",
+    id: 1,
+  },
+  {
+    title: "Assignment 2",
+    date: "2023-09-16",
+    priority: "High",
+    dueDate: "2023-09-18",
+    description: "Webware assignment 2",
+    id: 2,
+  },
+];
 
-]
-
-const server = http.createServer( function( request,response ) {
-  if( request.method === 'GET' ) {
-    handleGet( request, response )    
-  }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
+const server = http.createServer(function (request, response) {
+  if (request.method === "GET") {
+    handleGet(request, response);
+  } else if (request.method === "POST") {
+    handlePost(request, response);
+  } else if (request.method === "PUT") {
+    handlePut(request, response);
   }
-})
+});
 
-const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
+const handleGet = function (request, response) {
+  const filename = dir + request.url.slice(1);
 
-  if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
-  }else{
-    sendFile( response, filename )
+  if (request.url === "/") {
+    sendFile(response, "public/index.html");
+  } else {
+    sendFile(response, filename);
   }
-}
+};
 
-const handlePost = function( request, response ) {
-  let dataString = ''
+const handlePost = function (request, response) {
+  let dataString = "";
 
-  request.on( 'data', function( data ) {
-      dataString += data 
-  })
+  request.on("data", function (data) {
+    dataString += data;
+  });
 
-  request.on( 'end', function() {
-    const parsedData = JSON.parse(dataString)
-    console.log( parsedData )
+  request.on("end", function () {
+    const parsedData = JSON.parse(dataString);
+    console.log(parsedData);
 
-    // ... do something with the data here!!!
+    if (request.url === "/submit") {
+      const originalDate = new Date(parsedData.date);
+      let due = null;
 
-    appdata.push(
-      {
-        ...parsedData,
-        id: appdata.length+1
+      console.log(typeof parsedData.priority);
+      switch (parsedData.priority) {
+        case "Low":
+          due = addDays(originalDate, 5).toISOString().split("T")[0];
+          break;
+        case "Medium":
+          due = addDays(originalDate, 3).toISOString().split("T")[0];
+          break;
+        case "High":
+          due = addDays(originalDate, 1).toISOString().split("T")[0];
+          break;
       }
-    )
 
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end(JSON.stringify(appdata))
-  })
+      console.log(due);
+      appdata.push({
+        ...parsedData,
+        dueDate: due,
+        id: appdata.length + 1,
+      });
+    } else if (request.url === "/init") {
+    } else if (request.url === "/delete") {
+    }
+
+    response.writeHead(200, "OK", { "Content-Type": "text/plain" });
+    response.end(JSON.stringify(appdata));
+  });
+};
+
+const handlePut = function (request, response) {
+  console.log(request);
+};
+
+const sendFile = function (response, filename) {
+  const type = mime.getType(filename);
+
+  fs.readFile(filename, function (err, content) {
+    // if the error = null, then we've loaded the file successfully
+    if (err === null) {
+      // status code: https://httpstatuses.com
+      response.writeHeader(200, { "Content-Type": type });
+      response.end(content);
+    } else {
+      // file not found, error code 404
+      response.writeHeader(404);
+      response.end("404 Error: File Not Found");
+    }
+  });
+};
+
+server.listen(process.env.PORT || port);
+
+function addDays(date, days) {
+  var result = new Date(date);
+  result.setDate(result.getDate() + days);
+  return result;
 }
-
-const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
-
-   fs.readFile( filename, function( err, content ) {
-
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
-
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
-
-     }else{
-
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
-
-     }
-   })
-}
-
-server.listen( process.env.PORT || port )
