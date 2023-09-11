6dad1ee60461f0f158883177335c500e8c768087
Sarah Olson <saraholson5348@gmail.com>
2023-09-11 00:22:55 -0400

    semi-final iteration. README complete, basic requirements met, only 1 user test in README

--- public/js/main.js
+++ public/js/main.js
@@ -1,156 +1,153 @@
-// FRONT-END (CLIENT) JAVASCRIPT HERE
 
-/*const submit = async function( event ) {
-  // stop form submission from trying to load
-  // a new .html page for displaying results...
-  // this was the original browser behavior and still
-  // remains to this day
-  event.preventDefault()
-  
-  const input = document.querySelector( '#yourname' ),
-        json = { yourname: input.value },
-        body = JSON.stringify( json )
 
-  const response = await fetch( '/submit', {
-    method:'POST',
-    body 
-  })
-
-  const res = await response.json()
-  
-
-  console.log( res )
-
-  const list = document.createElement('ul')
-
-  res.forEach( d => {
-    const item = document.createElement('ul');
-    item.innerHTML = `<b>Model</b> : ${d.model}`
-    console.log(d.model)
-    list.appendChild(item)
-  })
-
-  document.body.appendChild(list)
-
-}*/
-
-window.onload = function() {
+window.onload = function () {
   //const button = document.querySelector("button");
   //button.onclick = submit;
 
-  const submitInfo = document.querySelector( "#enter" );
+  const submitInfo = document.querySelector("#enter");
   submitInfo.onclick = addHours
 
-  const setGoalGrade = document.querySelector( "#setGoal" );
+  const setGoalGrade = document.querySelector("#setGoal");
   setGoalGrade.onclick = setGoal
-
 
 }
 
-const setGoal = async function( event ) {
+const getResponse = async function (event) {
   event.preventDefault()
 
-  const goal = document.querySelector( '#desiredGoal' ),
-        json = { goal: goal.value }
-        body = JSON.stringify( json )
-  
-  const response = await fetch( '/setGoal' , {
-    method:'POST',
+  const response = await fetch('/getTable', {
+    method: 'GET',
+  })
+
+  const res = await response.json()
+  return res
+}
+
+const drawTable = function (res) {
+  const table = document.querySelector('tbody')
+
+  table.innerHTML = ''
+
+  let idx = 0;
+
+  res.forEach(d => {
+
+    const newRow = document.createElement('tr')
+
+    for (let r in d) {
+      const element = document.createElement('td')
+
+      console.log(d)
+      if(d[r] <= 0){
+        element.innerText = 0
+      } else {
+        element.innerText = d[r]    
+      }
+      newRow.appendChild(element)
+    }
+
+    const buttonBox = document.createElement('td')
+
+    const deleteButton = document.createElement('button')
+    deleteButton.innerText = "Delete"
+    let currIdx = idx
+    deleteButton.onclick = function (event) { deleteEntry(event, currIdx) }
+    buttonBox.appendChild(deleteButton)
+    newRow.appendChild(buttonBox)
+
+    table.appendChild(newRow)
+    idx += 1
+  })
+}
+
+const setGoal = async function (event) {
+  event.preventDefault()
+
+  const goal = document.querySelector('#desiredGoal'),
+    json = { goal: goal.value }
+  body = JSON.stringify(json)
+
+  const response = await fetch('/setGoal', {
+    method: 'POST',
     body
   })
 
   const res = await response.json()
-  console.log("res", res, res.goal)
 
-  const displayGoal = document.querySelector( '#termGradeGoal' )
-  
+  const displayGoal = document.querySelector('#termGradeGoal')
+
   displayGoal.innerText = res.goal
   displayGoal.className = res.goal
-  /*
-  const termGoal = document.createElement('p')
-  termGoal.innerText = res.goal
-  termGoal.className = res.goal
-  displayGoal.appendChild(termGoal)
-  */
+
   const editGoalButton = document.createElement('button')
   editGoalButton.onclick = editGoal
   editGoalButton.innerText = "Change Goal"
 
   goal.style.visibility = "hidden"
-  document.querySelector( '#setGoal' ).style.visibility = "hidden"
-  
-  const editGoalForm = document.querySelector( '#editGoalForm' )
+  document.querySelector('#setGoal').style.visibility = "hidden"
+
+  const editGoalForm = document.querySelector('#editGoalForm')
   editGoalForm.style.visibility = "visible"
   editGoalForm.appendChild(editGoalButton)
 
 }
 
-const editGoal = async function( event ) {
+const editGoal = async function (event) {
   event.preventDefault()
 
-  const goal = document.querySelector( '#newDesiredGoal' ),
-        json = { goal: goal.value }
-        body = JSON.stringify( json )
-  
-  const response = await fetch( '/editGoal' , {
-    method:'POST',
+  const goal = document.querySelector('#newDesiredGoal'),
+    json = { goal: goal.value }
+  body = JSON.stringify(json)
+
+  const response = await fetch('/editGoal', {
+    method: 'POST',
     body
   })
 
   const res = await response.json()
-  console.log("res", res, res.goal)
 
-  const displayGoal = document.querySelector( '#termGradeGoal' )
-  
-  displayGoal.innerText = res.goal
-  displayGoal.className = res.goal
+  const displayGoal = document.querySelector('#termGradeGoal')
+
+  displayGoal.innerText = res.newGoal.goal
+  displayGoal.className = res.newGoal.goal
+
+  drawTable(res.appdata)
 }
 
 
-const addHours = async function( event ) {
+const addHours = async function (event) {
 
   event.preventDefault()
 
-  const input_date = document.querySelector( '#date' ),
-        input_hours = document.querySelector( '#hours' ),
-        json = { date: input_date.value, hours: input_hours.value },
-        body = JSON.stringify( json )
+  const input_date = document.querySelector('#date'),
+    input_hours = document.querySelector('#hours'),
+    json = { date: input_date.value, hours: input_hours.value },
+    body = JSON.stringify(json)
 
-  const response = await fetch( '/addHours', {
-    method:'POST',
-    body 
+  const response = await fetch('/addHours', {
+    method: 'POST',
+    body
   })
 
   const res = await response.json()
-  console.log("res: ", res)
-  console.log("lOL")
 
-  const table = document.querySelector( '#badmintonDisplay' )
+  drawTable(res)
 
-  document.querySelector("#badmintonDisplay tr").remove
-  //table.innerHTML = ''
+}
 
-  res.forEach( d => {
+const deleteEntry = async function (event, index) {
+  event.preventDefault()
 
-    const newRow = document.createElement('tr')
+  const json = { idx: index },
+    body = JSON.stringify(json)
 
-    for(let r in d){
-      const element = document.createElement('td')
-      
-      //handling having negative time remaining
-      if(d === 'remaining' && d[r] <= 0){
-        element.innerText = 0
-      } else {
-        element.innerText = d[r]
-      }
-  
-  
-      newRow.appendChild(element) 
-    }
-
-    table.appendChild(newRow)
+  const response = await fetch('/deleteEntry', {
+    method: 'POST',
+    body
   })
 
+  const res = await response.json()
 
+  drawTable(res)
 
 }
--- server.improved.js
+++ server.improved.js
@@ -1,148 +1,170 @@
-const http = require( 'http' ),
-      fs   = require( 'fs' ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library if you're testing this on your local machine.
-      // However, Glitch will install it automatically by looking in your package.json
-      // file.
-      mime = require( 'mime' ),
-      dir  = 'public/',
-      port = 3000
+const http = require('http'),
+  fs = require('fs'),
+  // IMPORTANT: you must run `npm install` in the directory for this assignment
+  // to install the mime library if you're testing this on your local machine.
+  // However, Glitch will install it automatically by looking in your package.json
+  // file.
+  mime = require('mime'),
+  dir = 'public/',
+  port = 3000
 
-/*const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
-]*/
 
 const appdata = [
-  
+
 ]
 
-const hoursTilGrade = [ {'A': 28} , {'B': 24}, {'C': 21}]
+const hoursTilGrade = [{ 'A': 28 }, { 'B': 24 }, { 'C': 21 }]
 
 let hoursTilGoal = 0
 let currentGoal = ''
 
-const server = http.createServer( function( request,response ) {
-  if( request.method === 'GET' ) {
-    handleGet( request, response )    
-  }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
+const server = http.createServer(function (request, response) {
+  if (request.method === 'GET') {
+    handleGet(request, response)
+  } else if (request.method === 'POST') {
+    handlePost(request, response)
   }
 })
 
-const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
+const handleGet = function (request, response) {
+  const filename = dir + request.url.slice(1)
 
-  if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
-  }else{
-    sendFile( response, filename )
+  if (request.url === '/') {
+    sendFile(response, 'public/index.html')
+  }
+
+  else if (request.url === '/getTable') {
+    response.writeHead(200, "OK", { 'Content-Type': 'text/json' })
+    response.end(JSON.stringify(appdata))
+  }
+
+  else {
+    sendFile(response, filename)
   }
 }
 
-const handlePost = function( request, response ) {
+const handlePost = function (request, response) {
   let dataString = ''
 
-  request.on( 'data', function( data ) {
-      dataString += data 
+  request.on('data', function (data) {
+    dataString += data
   })
-  
-  if( request.url === '/setGoal'){
-    request.on( 'end', function() {
-      const currGoal = JSON.parse( dataString )
-      console.log(currGoal)
 
-      hoursTilGrade.forEach( d => {
-        for(let g in d){
-          if(g === currGoal.goal){
+  if (request.url === '/setGoal') {
+    request.on('end', function () {
+      const currGoal = JSON.parse(dataString)
+
+      hoursTilGrade.forEach(d => {
+        for (let g in d) {
+          if (g === currGoal.goal) {
             hoursTilGoal = d[g]
           }
         }
       })
-        currentGoal = hoursTilGoal
+      currentGoal = hoursTilGoal
 
-      response.writeHead( 200, "OK", {'Content-Type': 'text/json' })
-      response.end( JSON.stringify( currGoal ) )
-  })
- }
+      response.writeHead(200, "OK", { 'Content-Type': 'text/json' })
+      response.end(JSON.stringify(currGoal))
+    })
+  }
 
- else if( request.url === '/editGoal' ){
-  request.on( 'end', function() {
-    const newGoal = JSON.parse( dataString )
-    console.log(newGoal)
+  else if (request.url === '/editGoal') {
+    request.on('end', function () {
+      const newGoal = JSON.parse(dataString)
 
-    let newGoalNum = 0
+      let newGoalNum = 0
 
-    let differenceToChange = 0
-    
-    hoursTilGrade.forEach( d => {
-      for(let g in d){
-        if(g === newGoal.goal){
-          newGoalNum = d[g]
-          differenceToChange = newGoalNum - currentGoal 
-          console.log(d[g], differenceToChange, currentGoal)
-        }
+      let differenceToChange = 0
+
+      if (newGoal.goal !== currentGoal) {
+
+        hoursTilGrade.forEach(d => {
+          for (let g in d) {
+            if (g === newGoal.goal) {
+              newGoalNum = d[g]
+              differenceToChange = newGoalNum - currentGoal
+            }
+          }
+        })
+
+        hoursTilGoal += differenceToChange
+
+        currentGoal = newGoalNum
+
+        //go thru and edit all of the remianings
+        appdata.forEach(d => {
+          d.remaining += differenceToChange
+
+          // if (d.remaining <= 0) {
+          //   d.remaining = 0
+          // }
+        })
       }
+
+      response.writeHead(200, "OK", { 'Content-Type': 'text/json' })
+      response.end(JSON.stringify({ newGoal, appdata }))
     })
+  }
 
-    console.log(newGoalNum, differenceToChange)
-    
-    hoursTilGoal += differenceToChange
-    currentGoal = newGoalNum
+  else if (request.url === '/deleteEntry') {
+    request.on('end', function () {
+      const indexObj = JSON.parse(dataString)
+      const index = indexObj.idx
+      
+      console.log(appdata)
 
-    //go thru and edit all of the remianings
-    appdata.forEach( d => {
-      d.remaining += differenceToChange
+      //need to edit all entries after the index
+      let differenceToChange = parseFloat(appdata[index].hours)
+
+      for (let i = index; i < appdata.length; i++) {
+        appdata[i].remaining += differenceToChange
+      }
+
+      appdata.splice(parseInt(index), 1)
+
+
+      response.writeHead(200, "OK", { 'Content-Type': 'text/json' })
+      response.end(JSON.stringify(appdata))
     })
+  }
 
-    console.log(appdata)
-
-    response.writeHead( 200, "OK", {'Content-Type': 'text/json' })
-    response.end( JSON.stringify( newGoal ) )
-})
- }
-
-  else if ( request.url === '/addHours' ){
-    request.on( 'end', function() {
-      const currentData = JSON.parse( dataString ) 
+  else if (request.url === '/addHours') {
+    request.on('end', function () {
+      const currentData = JSON.parse(dataString)
 
       hoursTilGoal -= currentData.hours
+
       currentData.remaining = hoursTilGoal
-  
-      console.log(currentData)
+      
 
       appdata.push(currentData)
-      
-  
-      response.writeHead( 200, "OK", {'Content-Type': 'text/json' })
-      response.end( JSON.stringify(appdata) )
 
-      //response.end( JSON.stringify(currentData) )
+      response.writeHead(200, "OK", { 'Content-Type': 'text/json' })
+      response.end(JSON.stringify(appdata))
     })
   }
 }
 
-const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
+const sendFile = function (response, filename) {
+  const type = mime.getType(filename)
 
-   fs.readFile( filename, function( err, content ) {
+  fs.readFile(filename, function (err, content) {
 
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
+    // if the error = null, then we've loaded the file successfully
+    if (err === null) {
 
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
+      // status code: https://httpstatuses.com
+      response.writeHeader(200, { 'Content-Type': type })
+      response.end(content)
 
-     }else{
+    } else {
 
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
+      // file not found, error code 404
+      response.writeHeader(404)
+      response.end('404 Error: File Not Found')
 
-     }
-   })
+    }
+  })
 }
 
-server.listen( process.env.PORT || port )
+server.listen(process.env.PORT || port)
