228efee39181adab98b67af1c7034b27be608d3d
Mahir Sowad <32722691+mahirsowad3@users.noreply.github.com>
2023-09-11 10:25:21 -0400

    added getTasks functionality

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,41 @@
 // FRONT-END (CLIENT) JAVASCRIPT HERE
 
-const submit = async function( event ) {
+const submit = async function (event) {
   // stop form submission from trying to load
   // a new .html page for displaying results...
   // this was the original browser behavior and still
   // remains to this day
-  event.preventDefault()
-  
-  const input = document.querySelector( '#yourname' ),
-        json = { yourname: input.value },
-        body = JSON.stringify( json )
+  event.preventDefault();
 
-  const response = await fetch( '/submit', {
-    method:'POST',
-    body 
-  })
+  const form = document.querySelector("#addTaskForm");
+  const formData = new FormData(form);
+  // get all fields in variables
+  const taskName = formData.get("taskName");
+  const taskDescription = formData.get("taskDescription");
+  const taskDeadline = formData.get("taskDeadline");
+  const taskPriority = formData.get("taskPriority");
 
-  const text = await response.text()
+  console.log(formData.get("taskName"));
+  console.log(formData.get("taskDescription"));
+  console.log(formData.get("taskDeadline"));
+  console.log(typeof formData.get("taskDeadline"));
+  console.log(formData.get("taskPriority"));
 
-  console.log( 'text:', text )
-}
+  // const input = document.querySelector("#yourname"),
+  //   json = { yourname: input.value },
+  //   body = JSON.stringify(json);
 
-window.onload = function() {
-   const button = document.querySelector("button");
+  // const response = await fetch("/submit", {
+  //   method: "POST",
+  //   body,
+  // });
+
+  // const text = await response.text();
+
+  // console.log("text:", text);
+};
+
+window.onload = function () {
+  const button = document.getElementById("addTaskFormSubmitBtn");
   button.onclick = submit;
-}
+};
--- server.improved.js
+++ server.improved.js
@@ -1,74 +1,99 @@
-const http = require( 'http' ),
-      fs   = require( 'fs' ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library if you're testing this on your local machine.
-      // However, Glitch will install it automatically by looking in your package.json
-      // file.
-      mime = require( 'mime' ),
-      dir  = 'public/',
-      port = 3000
+const http = require("http"),
+  fs = require("fs"),
+  // IMPORTANT: you must run `npm install` in the directory for this assignment
+  // to install the mime library if you're testing this on your local machine.
+  // However, Glitch will install it automatically by looking in your package.json
+  // file.
+  mime = require("mime"),
+  dir = "public/",
+  port = 3000;
 
+/* 
+      // This is the general structure of the data:
+       taskObject = {
+          taskName: "name",
+          taskDescription: "description",
+          taskDeadline: "deadline",
+          taskPriority: "priority",
+        }; 
+      */
 const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
-]
+  {
+    taskName: "Clean the garage",
+    taskDescription:
+      "Throw away old junk in the trash. Reorganize items to clear up more floor space.",
+    taskDeadline: "2023-09-22",
+    taskPriority: "Medium",
+  },
+  {
+    taskName: "Wash the dishes",
+    taskDescription:
+      "Wash the dishes in the sink. Put them away in the cabinets.",
+    taskDeadline: "2023-09-14",
+    taskPriority: "High",
+  },
+  {
+    taskName: "Do the laundry",
+    taskDescription:
+      "Wash the clothes in the washing machine. Dry them in the dryer. Fold them and put them away.",
+    taskDeadline: "2023-09-20",
+    taskPriority: "Low",
+  },
+];
 
-const server = http.createServer( function( request,response ) {
-  if( request.method === 'GET' ) {
-    handleGet( request, response )    
-  }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
+const server = http.createServer(function (request, response) {
+  if (request.method === "GET") {
+    handleGet(request, response);
+  } else if (request.method === "POST") {
+    handlePost(request, response);
   }
-})
+});
 
-const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
+const handleGet = function (request, response) {
+  const filename = dir + request.url.slice(1);
 
-  if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
-  }else{
-    sendFile( response, filename )
+  if (request.url === "/") {
+    sendFile(response, "public/index.html");
+  } else if (request.url === "/getTasks") {
+    response.writeHead(200, "OK", { "Content-Type": "text/plain" });
+    response.end(JSON.stringify(appdata));
+  } else {
+    sendFile(response, filename);
   }
-}
+};
 
-const handlePost = function( request, response ) {
-  let dataString = ''
+const handlePost = function (request, response) {
+  let dataString = "";
 
-  request.on( 'data', function( data ) {
-      dataString += data 
-  })
+  request.on("data", function (data) {
+    dataString += data;
+  });
 
-  request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
+  request.on("end", function () {
+    console.log(JSON.parse(dataString));
 
     // ... do something with the data here!!!
 
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end('test')
-  })
-}
+    response.writeHead(200, "OK", { "Content-Type": "text/plain" });
+    response.end("test");
+  });
+};
 
-const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
+const sendFile = function (response, filename) {
+  const type = mime.getType(filename);
 
-   fs.readFile( filename, function( err, content ) {
+  fs.readFile(filename, function (err, content) {
+    // if the error = null, then we've loaded the file successfully
+    if (err === null) {
+      // status code: https://httpstatuses.com
+      response.writeHeader(200, { "Content-Type": type });
+      response.end(content);
+    } else {
+      // file not found, error code 404
+      response.writeHeader(404);
+      response.end("404 Error: File Not Found");
+    }
+  });
+};
 
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
-
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
-
-     }else{
-
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
-
-     }
-   })
-}
-
-server.listen( process.env.PORT || port )
+server.listen(process.env.PORT || port);
