8a5dd5942f5f28329757c3c9ad2c4d7442aab7b3
Glitch (a2-isabellapabon) <none>
2023-09-11 00:15:10 +0000

    Working commit

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,117 @@
 // FRONT-END (CLIENT) JAVASCRIPT HERE
 
-const submit = async function( event ) {
+/*const submit = async function (event) {
   // stop form submission from trying to load
   // a new .html page for displaying results...
   // this was the original browser behavior and still
   // remains to this day
-  event.preventDefault()
+  event.preventDefault();
+
+  const input = document.querySelector("#title"),
+    json = { title: input.value },
+    body = JSON.stringify(json);
+
+  const response = await fetch("/submit", {
+    method: "POST",
+    body,
+  });
+
+  const data = await response.json();
+
+  const list = document.createElement("ul");
+
+  data.forEach((d) => {
+    const item = document.createElement("li");
+    item.innerHTML = `<b>season</b> : ${d.season}, <b>title</b> : ${d.title}, <b>artist</b> : ${d.artist}`;
+    list.appendChild(item);
+  });
+
+  document.body.appendChild(list);
+};*/
+
+const add = async function (event) {
+  event.preventDefault();
+
+  const season = document.getElementById("season").value;
+  const songTitle = document.getElementById("title").value;
+  const songArtist = document.getElementById("artist").value;
+  const songLength = document.getElementById("length").value;
+  const json = {
+      season: season,
+      title: songTitle,
+      artist: songArtist,
+      length: songLength,
+    },
+    body = JSON.stringify(json);
+
+  const response = await fetch("/submit", {
+    method: "POST",
+    body,
+  });
+
+  const data = await response.json();
+  createPlaylist(data);
+};
+
+
+ function createPlaylist(data) {
+   
+  const list = document.getElementById("output");
+  const header = document.getElementById("header");
+   
+    list.innerHTML = "";
+    data.forEach((d) => {
+      let current_season = document.getElementById("season").value;
+      const item = document.createElement("p1");
+      const pop = document.createElement("button");
+      if (current_season === d.season) {
+        item.innerHTML = `${d.title} by ${d.artist}: ${d.length}`;
+        list.appendChild(item);
+
+        pop.innerHTML = `Delete`;
+        pop.onclick = async function (event) {
+          event.preventDefault();
+          const removeJson = {
+              season: d.season,
+              title: d.title,
+              artist: d.artist,
+              length: d.length,
+            },
+            removeBody = JSON.stringify(removeJson);
+
+          const removeResponse = await fetch("/remove", {
+            method: "POST",
+            body: removeBody,
+          });
+
+          const responseData = await removeResponse.json();
+          createPlaylist(responseData);
+        };
+        item.appendChild(pop);
+      } else {
+        document.getElementById("output").innerHTML = "";
+      }
+
+      header.innerHTML = `${d.season} playlist`;
+    });
+
+    document.body.appendChild(list);
+  }
+
+
+
+window.onload = async function () {
+  const addButton = document.getElementById("add");
+  addButton.onclick = add;
   
-  const input = document.querySelector( '#yourname' ),
-        json = { yourname: input.value },
-        body = JSON.stringify( json )
+  
+   const doesNothing = await fetch("/nothing", {
+            method: "POST",
+            body: JSON.stringify({})
+          });
+  
 
-  const response = await fetch( '/submit', {
-    method:'POST',
-    body 
-  })
+  // similar logic to add - not appending/replacing
+};
 
-  const text = await response.text()
-
-  console.log( 'text:', text )
-}
-
-window.onload = function() {
-   const button = document.querySelector("button");
-  button.onclick = submit;
-}
+//send data
--- server.improved.js
+++ server.improved.js
@@ -1,74 +1,121 @@
-const http = require( 'http' ),
-      fs   = require( 'fs' ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library if you're testing this on your local machine.
-      // However, Glitch will install it automatically by looking in your package.json
-      // file.
-      mime = require( 'mime' ),
-      dir  = 'public/',
-      port = 3000
+const http = require("http"),
+  fs = require("fs"),
+  // IMPORTANT: you must run `npm install` in the directory for this assignment
+  // to install the mime library if you're testing this on your local machine.
+  // However, Glitch will install it automatically by looking in your package.json
+  // file.
+  mime = require("mime"),
+  dir = "public/",
+  port = 3000;
 
-const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
-]
+const playlists = [
+  { season: "fall", title: "1901", artist: "Phoenix", length: "2:00" },
+];
 
-const server = http.createServer( function( request,response ) {
-  if( request.method === 'GET' ) {
-    handleGet( request, response )    
-  }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
+const server = http.createServer(function (request, response) {
+  if (request.method === "GET") {
+    handleGet(request, response);
+  } else if (request.method === "POST") {
+    handlePost(request, response);
   }
-})
+});
 
-const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
+const handleGet = function (request, response) {
+  const filename = dir + request.url.slice(1);
 
-  if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
-  }else{
-    sendFile( response, filename )
+  if (request.url === "/") {
+    sendFile(response, "public/index.html");
+  } else {
+    sendFile(response, filename);
   }
-}
+};
 
-const handlePost = function( request, response ) {
-  let dataString = ''
+const handlePost = function (request, response) {
+  let dataString = "";
+  request.on("data", function (data) {
+    dataString += data;
+  });
 
-  request.on( 'data', function( data ) {
-      dataString += data 
-  })
+  request.on("end", function () {
+    const res = JSON.parse(dataString);
 
-  request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
+    const season = res.season;
 
-    // ... do something with the data here!!!
+    const title = res.title;
 
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end('test')
-  })
-}
+    const artist = res.artist;
 
-const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
+    const length = res.length;
 
-   fs.readFile( filename, function( err, content ) {
+    if (request.url === "/submit") {
+      const minutes = Math.floor(length / 60).toString();
 
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
+      const seconds = (length % 60).toString();
 
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
+      let finalTime = "";
 
-     }else{
+      if (seconds < 10) {
+        finalTime = minutes + ":0" + seconds;
+      } else {
+        finalTime = minutes + ":" + seconds;
+      }
 
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
+      const obj = {
+        season: season,
+        title: title,
+        artist: artist,
+        length: finalTime,
+      };
 
-     }
-   })
-}
+      playlists.push(obj);
+    } else if(request.url === "/remove") {
+      const obj = {
+        season: season,
+        title: title,
+        artist: artist,
+        length: length,
+      };
 
-server.listen( process.env.PORT || port )
+      for (let i = 0; i < playlists.length; i++) {
+        if (
+          playlists[i].title === obj.title &&
+          playlists[i].artist === obj.artist &&
+          playlists[i].length === obj.length &&
+          playlists[i].season === obj.season
+        ) 
+        
+        {
+
+          playlists.splice(i, 1);
+          console.log(playlists);
+        }
+      }
+    }
+    else{
+      //does nothing!
+    }
+
+    response.writeHead(200, "OK", { "Content-Type": "text/json" });
+    response.end(JSON.stringify(playlists));
+  });
+};
+
+const sendFile = function (response, filename) {
+  const type = mime.getType(filename);
+
+  fs.readFile(filename, function (err, content) {
+    // if the error = null, then we've loaded the file successfully
+    if (err === null) {
+      // status code: https://httpstatuses.com
+      response.writeHeader(200, { "Content-Type": type });
+      response.end(content);
+    } else {
+      // file not found, error code 404
+      response.writeHeader(404);
+      response.end("Help! file not found");
+      console.log(err);
+    }
+  });
+};
+
+server.listen(process.env.PORT || port);
