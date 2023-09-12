26410ec8362d2dbf9201b35653d24d3a77be60a5
SmeeBoi <sameer.desai.boston@gmail.com>
2023-09-12 11:35:05 -0400

    added some comments. but still a loooot of repeated code :(

--- public/js/main.js
+++ public/js/main.js
@@ -2,73 +2,73 @@
 
 // CLIENT CODE
 
-let list = undefined;
-const submit = async function (event) {  
-  event.preventDefault(); // stop page from reloading when form is submitted
-
-  const name = document.querySelector('#yourname'), // get form input element with id=yourname
-    color = document.querySelector('#color')
-
-  let json = {}  // create json object
-  json.name = name.value // add name
-  json.color = color.value // add color
-  let min = Math.ceil(0);
-  let max = Math.floor(100);
-  json.score = Math.floor(Math.random() * (max - min) + min); // add random score for now
-
-  let clientData = JSON.stringify(json) // create json string from json object
-
-  const response = await fetch('/submit', { // send client data to server 
-    method: 'POST',
-    body: clientData // set body of request to clientData json string
-  })
-
-  let serverData = await response.json() // get json string from server response
-  console.log('SERVER DATA (after add player):')
-  console.log(serverData) // print json string to console
-   populateList(serverData);
+function randomNum(min, max) {
+  return Math.floor(Math.random() * (max - min +1)) + min;
 }
 
-function populateList(serverData) {
+let playerList = undefined;
+const submit = async function (event) {  
+  event.preventDefault(); // Stop page defualt reload behavior
 
-  // Remove list from body of html
-  list = document.getElementById('playerList');
-  if (list) {
-    list.remove();
+  const name = document.querySelector('#yourname'); // get form input element with id=yourname
+  const color = document.querySelector('#color');
+
+  let formData = {}  // json for client data
+  formData.name = name.value 
+  formData.color = color.value 
+  formData.score = randomNum(1,100); // Add random score between 1-100
+
+  const response = await fetch('/submit', { // Send client data to server 
+    method: 'POST',
+    body: JSON.stringify(formData) // Convert clientData to JSON string
+  })
+
+  let serverData = await response.json() // Get server response
+  console.log('SERVER DATA (after add player):')
+  console.log(serverData) // Print current server data
+  populateList(serverData); // Populate list of players in html with server data
+}
+
+// Populate list of players in html with server data
+function populateList(serverData) {
+  playerList = document.getElementById('playerList');
+  if (playerList) {
+    playerList.remove();  // Reset player list by removing old list
   }
 
-  list = document.createElement('ol'); // create ordered list element 
-  list.id = 'playerList';
-  document.body.appendChild(list); // append list to body of html
+  playerList = document.createElement('ol'); // Ordered list of players 
+  playerList.id = 'playerList';
+  document.body.appendChild(playerList);
 
-  serverData.forEach(d => { // for each element in json string, create a list item and append to list
+  // For each player in server data, create a list item
+  serverData.forEach(d => { 
+    const player = document.createElement('li') // Player list item
 
-    const item = document.createElement('li')
-
-    // add edit button
+    // Add edit button
     const editButton = document.createElement('button')
     editButton.innerHTML = 'edit'
     editButton.class = 'editButton'
 
-
-    // add delete button
+    // Add delete button
     const deleteButton = document.createElement('button')
     deleteButton.innerHTML = 'delete'
     deleteButton.class = 'deleteButton'
 
-    // Server Data to be displayed in list item
-    item.innerHTML = `Name: ${d.name}, Color: ${d.color}, Score: ${d.score}, Rank: ${d.rank}`
-    item.className = 'player'
-    item.id = `${d.name}`; // set id of list item to name of player
+    // Server Data to be displayed in list player
+    player.innerHTML = `Name: ${d.name}, Color: ${d.color}, Score: ${d.score}, Rank: ${d.rank}`
+    player.className = 'player'
+    player.id = `${d.name}`; // Set id of player list item to name of player
 
-    // Add buttons in li item
-    item.appendChild(editButton)
-    item.appendChild(deleteButton)
+    // Add buttons in li player
+    player.appendChild(editButton)
+    player.appendChild(deleteButton)
 
-    list.appendChild(item)
+    // Add player <li> to list <ol>
+    playerList.appendChild(player) 
   })
 }
 
+// When page loads, add event listeners to buttons
 window.onload = function () {
   const playbutton = document.querySelector('#playbutton');
   playbutton.onclick = submit;
@@ -77,8 +77,7 @@ window.onload = function () {
   document.addEventListener('click', function (e) {
     if (e.target && e.target.class == 'deleteButton') {
       let player = e.target.closest('.player');
-      // Call the deletePlayer function with the player name
-      deletePlayer(player.id);
+      deletePlayer(player.id); // player id is name of player
     }
   });
 
@@ -86,47 +85,46 @@ window.onload = function () {
   document.addEventListener('click', function (e) {
     if (e.target && e.target.class == 'editButton') {
       let player = e.target.closest('.player');
-      // Call the editPlayer function with the player name
-      editPlayer(player.id);
+      editPlayer(player.id); // player id is name of player
     }
   });
 }
 
+// Edit player name on server
 async function editPlayer(playerName) {
-  let newName = prompt("Please enter new name:", "New Name");
+  let newName = prompt("Enter new name:", "New Name"); // Get new name from user
   if (newName == null || newName == "") {
-    alert("Player name not changed");
+    alert("Player name not changed"); // If user cancels or enters empty string, do nothing
   } else {
-    let json = {}  // create json object
-    json.name = playerName // add name
-    json.newName = newName // add new name
+    let editPlayer = {}
+    editPlayer.name = playerName
+    editPlayer.newName = newName // New name to be changed to
 
     const response = await fetch('/edit', {
       method: 'PUT',
-      body: JSON.stringify(json)
+      body: JSON.stringify(editPlayer)
     })
 
-    let serverData = await response.json() // get json string from server response
+    let serverData = await response.json()
     console.log('SERVER DATA (after edit):');
-    console.log(serverData); // print json string to console
+    console.log(serverData);
     populateList(serverData);
   }
 }
 
 
-
+// Delete player from server
 async function deletePlayer(playerName) {
-  let json = {}  // create json object
-  json.name = playerName // add name
+  let deletePlayer = {}
+  deletePlayer.name = playerName // Player name to be deleted
 
-  // let playerNameJSON = JSON.stringify(playerName) // create json string 
   const response = await fetch('/delete', {
     method: 'DELETE',
-    body: JSON.stringify(json)
+    body: JSON.stringify(deletePlayer)
   })
 
-  let serverData = await response.json() // get json string from server response
+  let serverData = await response.json()
   console.log('SERVER DATA (after delete):');
-  console.log(serverData); // print json string to console
+  console.log(serverData);
   populateList(serverData);
 }
