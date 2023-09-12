fe308d4dc7ff5fa2ee33050482fe7110f32c1fa4
nsadlier <113724201+nsadlier@users.noreply.github.com>
2023-09-12 01:51:04 -0400

    A2 requirements implemented

--- public/js/main.js
+++ public/js/main.js
@@ -7,8 +7,10 @@ const submit = async function( event ) {
   // remains to this day
   event.preventDefault()
   
-  const input = document.querySelector( '#yourname' ),
-        json = { yourname: input.value },
+  const input1 = document.querySelector( '#name' ),
+        input2 = document.querySelector( '#age' ),
+        input3 = document.querySelector( '#year' ),
+        json = { name: input1.value, age: input2.value, year: input3.value},
         body = JSON.stringify( json )
 
   const response = await fetch( '/submit', {
@@ -17,11 +19,71 @@ const submit = async function( event ) {
   })
 
   const text = await response.text()
-
+  
   console.log( 'text:', text )
+
+  var table = makeTable(text)
+
+  results.replaceChild(table, results.childNodes[0])
+
+}
+
+const deleteEntry = async function( event ) {
+  event.preventDefault()
+
+  const input = document.querySelector( '#delete' ),
+        json = { index: input.value},
+        body = JSON.stringify( json )
+
+  const response = await fetch( '/deleteEntry', {
+    method:'DELETE',
+    body 
+  })
+
+  const text = await response.text()
+  
+  console.log( 'text2:', text )
+
+  var table = makeTable(text)
+
+  results.replaceChild(table, results.childNodes[0])
+}
+
+function makeTable(text) {
+  var tableData = JSON.parse(text)
+  results = document.getElementById("results")
+  table = document.createElement("table")
+  columns = ['Name', 'Age', 'Current Year', 'Birth Year']
+
+  thead = document.createElement("thead")
+  tr = document.createElement("tr")
+
+  columns.forEach((element) => {
+    th = document.createElement("th")
+    th.innerText = element
+    tr.appendChild(th)
+  });
+  thead.appendChild(tr)
+  table.appendChild(tr)
+
+  tableData.forEach((element) => {
+    tr = document.createElement("tr")
+    values = Object.values(element)
+
+    values.forEach((element) => {
+      td = document.createElement("td")
+      td.innerText = element
+      tr.appendChild(td)
+    })
+    table.appendChild(tr)
+  });
+
+  return table
 }
 
 window.onload = function() {
-   const button = document.querySelector("button");
+  const button = document.querySelector("#submit");
   button.onclick = submit;
+  const button2 = document.querySelector("#deleteButton")
+  button2.onclick = deleteEntry;
 }
--- server.improved.js
+++ server.improved.js
@@ -8,17 +8,15 @@ const http = require( 'http' ),
       dir  = 'public/',
       port = 3000
 
-const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
-]
+const appdata = []
 
 const server = http.createServer( function( request,response ) {
   if( request.method === 'GET' ) {
     handleGet( request, response )    
   }else if( request.method === 'POST' ){
     handlePost( request, response ) 
+  }else if( request.method === 'DELETE' ){
+    handleDelete( request, response ) 
   }
 })
 
@@ -40,14 +38,31 @@ const handlePost = function( request, response ) {
   })
 
   request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
-
-    // ... do something with the data here!!!
-
+    var data = JSON.parse( dataString )
+    data.birthyear = data.year - data.age - 1
+    appdata.push(data)
+    console.log( appdata )
     response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end('test')
+    response.end(JSON.stringify(appdata))
   })
 }
+
+const handleDelete = function( request, response ) {
+  let dataString = ''
+
+  request.on( 'data', function( data ) {
+      dataString += data 
+  })
+
+  request.on( 'end', function() {
+    var data = JSON.parse( dataString )
+    appdata.splice(data.index, 1)
+    console.log( data.index )
+    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
+    response.end(JSON.stringify(appdata))
+  })
+}
+
 
 const sendFile = function( response, filename ) {
    const type = mime.getType( filename ) 
