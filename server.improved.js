1490ce4d87dc900e341d304aeec083d1873ef3c8
Glitch (a2-sl-wu) <none>
2023-09-14 04:55:40 +0000

    🔠🧿 Updated with Glitch

--- server.improved.js
+++ server.improved.js
@@ -1,84 +1,82 @@
-const http = require( 'http' ),
-      fs   = require( 'fs' ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library if you're testing this on your local machine.
-      // However, Glitch will install it automatically by looking in your package.json
-      // file.
-      mime = require( 'mime' ),
-      dir  = 'public/',
-      port = 3000
+const http = require("http"),
+  fs = require("fs"),
+  mime = require("mime"),
+  dir = "public/",
+  port = 3000;
 
-const appdata = [
-  //{ date: '9/10/2023', exercise: 'Squats', sets: 3, reps: 5, weight: 145 }
-]
+const appdata = [];
 
-const server = http.createServer( function( request,response ) {
-  if( request.method === 'GET' ) {
-    handleGet( request, response )    
-  }else if( request.method === 'POST' ){
-    handlePost( request, response )
+const server = http.createServer(function (request, response) {
+  if (request.method === "GET") {
+    handleGet(request, response);
+  } else if (request.method === "POST") {
+    handlePost(request, response);
   }
-})
+});
 
-const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
+const handleGet = function (request, response) {
+  const filename = dir + request.url.slice(1);
 
-  if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
-  } /*else if (request.url === "/getLogs") {
+  if (request.url === "/") {
+    sendFile(response, "public/index.html");
+  } else if (request.url === "/fetchData") {
     response.writeHeader(200, { "Content-Type": "text/plain" });
     response.end(JSON.stringify(appdata));
-  } */else {
-    sendFile( response, filename )
+  } else {
+    sendFile(response, filename);
   }
-}
+};
 
-const handlePost = function( request, response ) {
-  let dataString = ''
+const handlePost = function (request, response) {
+  let dataString = "";
 
-  request.on( 'data', function( data ) {
-      dataString += data 
-  })
-  //console.log(JSON.parse(dataString))
+  request.on("data", function (data) {
+    dataString += data;
+  });
 
-  request.on( 'end', function() {
-    //if ( request.url === "/submit" ) {
+  request.on("end", function () {
+    if (request.url === "/submit") {
       appdata.push(JSON.parse(dataString));
-    /*} else if ( request.url === "/delete" ) {
-      console.log( JSON.parse( dataString ) )
-      handleDelete( JSON.parse(dataString) )
-    }*/
-    // ... do something with the data here!!!
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    // response.write(JSON.stringify(appdata))
-    response.end()
-  })
+    } else if (request.url === "/delete") {
+      handleDelete(JSON.parse(dataString));
+    }
+    for (let i = 0; i < appdata.length; i++) {
+      let response = appdata[i];
+      response.volume = calculateVolume(
+        response.sets,
+        response.reps,
+        response.weight
+      )
+    }
+    response.writeHead(200, "OK", { "Content-Type": "text/plain" });
+    response.end();
+  });
+};
+
+const handleDelete = function (data) {
+  appdata.splice(data["deleteResponse"], 1);
+};
+
+const sendFile = function (response, filename) {
+  const type = mime.getType(filename);
+
+  fs.readFile(filename, function (err, content) {
+    // if the error = null, then we've loaded the file successfully
+    if (err === null) {
+      // status code: https://httpstatuses.com
+      response.writeHeader(200, { "Content-Type": type });
+      response.end(content);
+    } else {
+      // file not found, error code 404
+      response.writeHeader(404);
+      response.end("404 Error: File Not Found");
+    }
+  });
+};
+
+server.listen(process.env.PORT || port);
+
+const calculateVolume = function (sets, reps, weight) {
+  let volume = sets * reps * weight;
+  return volume;
 }
-
-const handleDelete = function( data ) {
-    appdata.splice(data["deleteLog"], 1);
-}
-
-const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
-
-   fs.readFile( filename, function( err, content ) {
-
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
-
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
-
-     }else{
-
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
-
-     }
-   })
-}
-
-server.listen( process.env.PORT || port )
