e69d3084198686b04a4117b5f2e8d45d76392169
ssilkaitis <86628667+ssilkaitis@users.noreply.github.com>
2023-09-20 11:16:25 -0400

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
 // FRONT-END (CLIENT) JAVASCRIPT HERE
-
+let modifyData = false
+let modId = null
 const submit = async function (event) {
   // stop form submission from trying to load
   // a new .html page for displaying results...
@@ -8,17 +9,19 @@ const submit = async function (event) {
   event.preventDefault();
 
   const input = document.querySelectorAll("#name, #mail, #asnmt, #date, #msg"),
-    //add the date to the json and send to the server
     json = {
       name: input[0].value,
       mail: input[1].value,
       asnmt: input[2].value,
       date: input[3].value,
       msg: input[4].value,
-    },
-    body = JSON.stringify(json);
+    }
+  if (modifyData === true) { 
+    json.id === modId;
+  } 
+  const body = JSON.stringify(json);
 
-  fetch("/submit", {
+  fetch((modifyData === false) ? "/submit" : "/modify", {
     method: "POST",
     body,
   }).then(async function (response) {
@@ -28,21 +31,21 @@ const submit = async function (event) {
     console.log(data);
   });
 
-  //when doing assignment- don't send text to server--> do json instead
-  //create new file in the row with assignment, due date, calculate priority --> by date due
-};
+  };
 const list = document.createElement("ul");
 
 //render the priority field
 async function render(data) {
   list.innerHTML = "";
+  modifyData = false
+  modId = null
   const item1 = document.createElement("p");
   const button_sort = document.createElement("button");
-    button_sort.innerText = "Sort By Priority";
-    button_sort.onclick = function() {
-      prioritysort(data);
-    };
-  
+  button_sort.innerText = "Sort By Priority";
+  button_sort.onclick = function () {
+    prioritysort(data);
+  };
+
   item1.appendChild(button_sort);
   list.appendChild(item1);
   for (let i = 0; i < data.length; i++) {
@@ -54,7 +57,15 @@ async function render(data) {
     button.onclick = function () {
       del(d.id);
     };
+    const buttonmod = document.createElement("button");
+    buttonmod.innerText = "Modify";
+    buttonmod.id = 'btn' + d.id
+    buttonmod.onclick = function () {
+      mod(d);
+    };
+    
     item.appendChild(button);
+    item.appendChild(buttonmod);
     list.appendChild(item);
   }
 }
@@ -62,21 +73,21 @@ async function render(data) {
 async function prioritysort(data) {
   const sortedArray = [];
   console.log(data);
-  for(let i = 0; i < data.length; i++){
+  for (let i = 0; i < data.length; i++) {
     const d = data[i];
-    if(d.priority == 'High'){
+    if (d.priority == "High") {
       sortedArray.push(data[i]);
     }
   }
-  for(let i = 0; i < data.length; i++){
+  for (let i = 0; i < data.length; i++) {
     const d = data[i];
-    if(d.priority == 'Medium'){
+    if (d.priority == "Medium") {
       sortedArray.push(data[i]);
     }
   }
-  for(let i = 0; i < data.length; i++){
+  for (let i = 0; i < data.length; i++) {
     const d = data[i];
-    if(d.priority == 'Low'){
+    if (d.priority == "Low") {
       sortedArray.push(data[i]);
     }
   }
@@ -94,6 +105,26 @@ async function del(id) {
     render(data);
     console.log(data);
   });
+}
+
+async function mod(data) {
+  modifyData = true
+  modId = data.id
+  document.getElementById('name').value = data.name;
+  document.getElementById('mail').value = data.mail;
+  document.getElementById('asnmt').value = data.asnmt;
+  document.getElementById('date').value = data.date;
+  document.getElementById('msg').value = data.msg;
+  
+  /*fetch("/modify", {
+    method: "POST",
+    body: data,
+  }).then(async function (response) {
+    let data = await response.json();
+    data.forEach((entry, index) => (entry.id = index));
+    render(data);
+    console.log(data);
+  });*/
 }
 
 window.onload = function () {
